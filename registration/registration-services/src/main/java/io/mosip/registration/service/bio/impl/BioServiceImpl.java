22ee9bf2035ff716fe2064d8da426578e73663aa
yaswanths2 <yaswanth.s2@mindtree.com>
2020-02-20 15:18:02 +0530

    MOSIP-180 : Reverted back to mdm

--- registration/registration-services/src/main/java/io/mosip/registration/service/bio/impl/BioServiceImpl.java
+++ registration/registration-services/src/main/java/io/mosip/registration/service/bio/impl/BioServiceImpl.java
@@ -450,6 +450,7 @@ public class BioServiceImpl extends BaseService implements BioService {
 	 */
 	@Override
 	public boolean isMdmEnabled() {
+		
 		return false;
 //		return RegistrationConstants.ENABLE
 //				.equalsIgnoreCase(((String) ApplicationContext.map().get(RegistrationConstants.MDM_ENABLED)));
