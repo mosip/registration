4fd9c5fa990d4b114a7d64cb3d5dbea305dafa69
mosipsa1 <mosipsa1@mindtree.com>
2020-02-27 12:22:28 +0530

    MOS-30434 : Reverted isMDM check

--- registration/registration-services/src/main/java/io/mosip/registration/service/bio/impl/BioServiceImpl.java
+++ registration/registration-services/src/main/java/io/mosip/registration/service/bio/impl/BioServiceImpl.java
@@ -450,11 +450,10 @@ public class BioServiceImpl extends BaseService implements BioService {
 	@Override
 	public boolean isMdmEnabled() {
 
-		return false;
-
-		// return RegistrationConstants.ENABLE
-		// .equalsIgnoreCase(((String)
-		// ApplicationContext.map().get(RegistrationConstants.MDM_ENABLED)));
+		
+		 return RegistrationConstants.ENABLE
+		 .equalsIgnoreCase(((String)
+		 ApplicationContext.map().get(RegistrationConstants.MDM_ENABLED)));
 	}
 
 	/*
