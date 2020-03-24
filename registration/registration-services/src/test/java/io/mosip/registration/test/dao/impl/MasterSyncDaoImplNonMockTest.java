c947577eb178a5e23b382f2f97d8439f1736c100
ase-101 <>
2020-03-24 20:44:26 +0530

    sync fixes MOSIP-279, changed the field name dproviderId in MosipDeviceService and handled byte array & timestamp field type.

--- registration/registration-services/src/test/java/io/mosip/registration/test/dao/impl/MasterSyncDaoImplNonMockTest.java
+++ registration/registration-services/src/test/java/io/mosip/registration/test/dao/impl/MasterSyncDaoImplNonMockTest.java
@@ -1,14 +1,20 @@
 package io.mosip.registration.test.dao.impl;
 
+import static org.junit.Assert.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -17,10 +23,16 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 import io.mosip.registration.constants.RegistrationConstants;
 import io.mosip.registration.context.ApplicationContext;
+import io.mosip.registration.dao.MachineMappingDAO;
 import io.mosip.registration.dao.MasterSyncDao;
 import io.mosip.registration.dto.response.SyncDataResponseDto;
+import io.mosip.registration.entity.MachineMaster;
+import io.mosip.registration.entity.UserDetail;
 import io.mosip.registration.repositories.BiometricAttributeRepository;
 import io.mosip.registration.repositories.BiometricTypeRepository;
+import io.mosip.registration.repositories.MachineMasterRepository;
+import io.mosip.registration.repositories.RegistrationCenterUserRepository;
+import io.mosip.registration.repositories.UserDetailRepository;
 import io.mosip.registration.test.config.TestDaoConfig;
 
 
@@ -44,6 +56,15 @@ public class MasterSyncDaoImplNonMockTest {
 	@Autowired
 	private BiometricTypeRepository biometricTypeRepository;
 	
+	@Autowired
+	private UserDetailRepository userDetailRepository;
+	
+	@Autowired
+	private MachineMappingDAO machineMappingDAO;
+	
+	@Autowired
+	private MachineMasterRepository machineMasterRepository;
+		
 	
 	@BeforeClass
 	public static void setup() throws Exception {		
@@ -53,41 +74,64 @@ public class MasterSyncDaoImplNonMockTest {
 	  appMap.put(RegistrationConstants.FINGERPRINT_DISABLE_FLAG, "Y");
 	  appMap.put(RegistrationConstants.IRIS_DISABLE_FLAG, "Y");
 	  appMap.put(RegistrationConstants.FACE_DISABLE_FLAG, "Y");
-	  appMap.put(RegistrationConstants.PRIMARY_LANGUAGE, "ara");
+	  appMap.put(RegistrationConstants.PRIMARY_LANGUAGE, "eng");
 	  appMap.put(RegistrationConstants.SECONDARY_LANGUAGE, "fra");
 	  appMap.put(RegistrationConstants.TPM_AVAILABILITY, RegistrationConstants.DISABLE);
-	  //ApplicationContext.getInstance().setApplicationMap(appMap);
-	  ApplicationContext.getInstance().loadResourceBundle();		 
-		
+	  ApplicationContext.getInstance().setApplicationMap(appMap);
+	  ApplicationContext.getInstance().loadResourceBundle();
+	}
+	
+	private void mockRegcenterUserMapping() {
+		List<UserDetail> users = new ArrayList<UserDetail>();
+		UserDetail userDetail1 = new UserDetail();
+		userDetail1.setId("110022");
+		userDetail1.setRegid("10003");
+		users.add(userDetail1);
+		UserDetail userDetail2 = new UserDetail();
+		userDetail2.setId("110024");
+		userDetail2.setRegid("10003");
+		users.add(userDetail2);
+		UserDetail userDetail3 = new UserDetail();
+		userDetail3.setId("110003");
+		userDetail3.setRegid("10003");
+		users.add(userDetail3);
+		UserDetail userDetail4 = new UserDetail();
+		userDetail4.setId("110002");
+		userDetail4.setRegid("10003");
+		users.add(userDetail4);
+		userDetailRepository.saveAll(users);
 	}
 	
 	@Test
 	public void testSuccessBiometricSave() {
 		SyncDataResponseDto syncDataResponseDto = getSyncDataResponseDto("biometricJson.json");	
 		
-		assertEquals(2, syncDataResponseDto.getDataToSync().size());
-		assertEquals(2, syncDataResponseDto.getDataToSync().get(0).getData().size());
-		assertEquals(2, syncDataResponseDto.getDataToSync().get(1).getData().size());
-			
 		String response = masterSyncDaoImpl.saveSyncData(syncDataResponseDto);
 		
-		assertEquals(2, biometricTypeRepository.count());
-		assertEquals(2, biometricAttributeRepository.count());
 		assertEquals(RegistrationConstants.SUCCESS, response);
 	}
 	
 	@Test
 	public void testSuccessSave() {
 		String response=null;
-		SyncDataResponseDto syncDataResponseDto = getSyncDataResponseDto("responseJson.json");	
-				
+		SyncDataResponseDto syncDataResponseDto = getSyncDataResponseDto("responseJson.json");
+		
 		response = masterSyncDaoImpl.saveSyncData(syncDataResponseDto);
 		
 		assertEquals(RegistrationConstants.SUCCESS, response);
+								
+		MachineMaster machine = machineMasterRepository.findByIsActiveTrueAndNameAndRegMachineSpecIdLangCode("b2ml24784", "eng");
+		 
+		assertNotNull(machine);
+		
+		String keyIndexBasedOnMachineName = machineMappingDAO.getKeyIndexByMachineName("b2ml24784");
+		assertNotNull(keyIndexBasedOnMachineName);
 	}
 	
 	
-	private SyncDataResponseDto getSyncDataResponseDto(String fileName) {		
+	private SyncDataResponseDto getSyncDataResponseDto(String fileName) {
+		mockRegcenterUserMapping();
+		
 		ObjectMapper mapper = new ObjectMapper();
         SyncDataResponseDto syncDataResponseDto = null;
 		
