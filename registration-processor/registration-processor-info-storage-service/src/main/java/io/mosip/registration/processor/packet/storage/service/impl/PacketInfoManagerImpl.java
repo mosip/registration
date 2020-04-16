67edf00dd0ffbae07c9423550cb6459d5fdeb34e
Sowmya Ujjappa Banakar <43202851+sowmya695@users.noreply.github.com>
2020-04-16 10:44:30 +0530

    Issue/mos 24767 (#121)
    * Merge pull request #1385 from mosip/sowmya_MOS-24767
    
    Sowmya mos 24767
    
    * MOS-24767 changes
    
    * MOS-24767 added snapshots
    
    Co-authored-by: Monobikash Das <43202165+MonobikashDas@users.noreply.github.com>
    Co-authored-by: M1022006 <M1022006@A2ML18485.mindtree.com>

--- registration-processor/registration-processor-info-storage-service/src/main/java/io/mosip/registration/processor/packet/storage/service/impl/PacketInfoManagerImpl.java
+++ registration-processor/registration-processor-info-storage-service/src/main/java/io/mosip/registration/processor/packet/storage/service/impl/PacketInfoManagerImpl.java
@@ -213,11 +213,24 @@ public class PacketInfoManagerImpl implements PacketInfoManager<Identity, Applic
 					jsonValueList.add(nameArray);
 			}
 			demographicData.setName(jsonValueList.isEmpty() ? null : jsonValueList);
-			
+
 			demographicData.setDateOfBirth((String) JsonUtil.getJSONValue(demographicIdentity,
 					JsonUtil.getJSONValue(JsonUtil.getJSONObject(regProcessorIdentityJson, "dob"), "value")));
 			demographicData.setGender(JsonUtil.getJsonValues(demographicIdentity,
 					JsonUtil.getJSONValue(JsonUtil.getJSONObject(regProcessorIdentityJson, "gender"), "value")));
+			demographicData.setPhone((String) JsonUtil.getJSONValue(demographicIdentity,
+					JsonUtil.getJSONValue(JsonUtil.getJSONObject(regProcessorIdentityJson, "phone"), "value")));
+			demographicData.setEmail((String) JsonUtil.getJSONValue(demographicIdentity,
+					JsonUtil.getJSONValue(JsonUtil.getJSONObject(regProcessorIdentityJson, "email"), "value")));
+			String[] addressList = ((String) JsonUtil
+					.getJSONValue(JsonUtil.getJSONObject(regProcessorIdentityJson, "address"), "value")).split(",");
+			for (String address : addressList) {
+				if (address.equals("postalCode")) {
+					demographicData.setPostalCode((String) JsonUtil.getJSONValue(demographicIdentity, address));
+				}
+					
+			}
+
 		} catch (IOException e) {
 			regProcLogger.error(LoggerFileConstant.SESSIONID.toString(), LoggerFileConstant.REGISTRATIONID.toString(),
 					"", e.getMessage() + ExceptionUtils.getStackTrace(e));
