e9a656cf6b2582255aaec77ac206bc19e634c8b4
M1022006 <M1022006@A2ML18485.mindtree.com>
2020-04-23 11:40:31 +0530

    MOSIP-751 Removed scaledScore in Modalities

--- registration-processor/registration-processor-core/src/main/java/io/mosip/registration/processor/core/packet/dto/abis/Modalities.java
+++ registration-processor/registration-processor-core/src/main/java/io/mosip/registration/processor/core/packet/dto/abis/Modalities.java
@@ -1,7 +1,6 @@
 package io.mosip.registration.processor.core.packet.dto.abis;
 
 import java.io.Serializable;
-import java.util.Arrays;
 
 import lombok.Data;
 
@@ -12,7 +11,6 @@ public class Modalities implements Serializable{
 	 */
 	private static final long serialVersionUID = 785216354868291150L;
     private String biometricType;
-    private String scaledScore;
     private Analytics analytics;
 
 }
