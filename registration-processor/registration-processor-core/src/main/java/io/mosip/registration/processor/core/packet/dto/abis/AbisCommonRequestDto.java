0ed2f3c1c2d9292e0adbed43a876aa404354e065
M1022006 <M1022006@A2ML18485.mindtree.com>
2020-04-23 20:34:17 +0530

    MOSIP-751 Added Deserializer serializer annotation

--- registration-processor/registration-processor-core/src/main/java/io/mosip/registration/processor/core/packet/dto/abis/AbisCommonRequestDto.java
+++ registration-processor/registration-processor-core/src/main/java/io/mosip/registration/processor/core/packet/dto/abis/AbisCommonRequestDto.java
@@ -3,7 +3,11 @@ package io.mosip.registration.processor.core.packet.dto.abis;
 import java.io.Serializable;
 import java.time.LocalDateTime;
 
-import org.apache.htrace.shaded.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 
 import lombok.Data;
 
@@ -24,6 +28,8 @@ public class AbisCommonRequestDto implements Serializable{
 	/** The request id. */
 	private String requestId;
 	
+	@JsonDeserialize(using = LocalDateTimeDeserializer.class)
+	@JsonSerialize(using = LocalDateTimeSerializer.class)
 	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'")
 	private LocalDateTime requesttime;
 	
