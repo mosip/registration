d8224efa85678df41d328513499321122c50b44e
sadanandegowda <43214830+sadanandegowda@users.noreply.github.com>
2020-04-27 19:26:11 +0530

    [MOSIP-916] - DB changes for the ABIS new specs (#149)
    * [MOSIP-916] - DB changes for the ABIS new specs
    
    * [MOSIP-723]- ID Schema related DB table scripts and deployment scripts

---
+++ db_scripts/mosip_reg/ddl/reg-schema_definition.sql
@@ -0,0 +1,30 @@
+-- -------------------------------------------------------------------------------------------------
+-- Database Name: mosip_reg
+-- Table Name 	: 
+-- Purpose    	: 
+--           
+-- Create By   	: Sadanandegowda DM
+-- Created Date	: 22-Apr-2019
+-- 
+-- Modified Date        Modified By         Comments / Remarks
+-- ------------------------------------------------------------------------------------------
+-- 
+-- ------------------------------------------------------------------------------------------
+-- object: reg.schema_definition | type: TABLE --
+-- DROP TABLE IF EXISTS reg.schema_definition CASCADE;
+CREATE TABLE reg.schema_definition(
+	id character varying(36) NOT NULL,
+	def_type character varying(16),
+	def_name character varying(36),
+	add_props boolean,
+	def_json character varying(4086),
+	is_active boolean NOT NULL,
+	cr_by character varying(256) NOT NULL,
+	cr_dtimes timestamp NOT NULL,
+	upd_by character varying(256),
+	upd_dtimes timestamp,
+	is_deleted boolean,
+	del_dtimes timestamp,
+	CONSTRAINT pk_schdef_id PRIMARY KEY (id)
+
+);
