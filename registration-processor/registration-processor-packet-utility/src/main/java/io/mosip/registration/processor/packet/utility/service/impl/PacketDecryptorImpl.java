31c438552face25124af3fe89d85fa569ca145aa
M1022006 <M1022006@A2ML18485.mindtree.com>
2020-05-08 11:26:09 +0530

    MOSIP-1040 resttemplate code commented

--- registration-processor/registration-processor-packet-utility/src/main/java/io/mosip/registration/processor/packet/utility/service/impl/PacketDecryptorImpl.java
+++ registration-processor/registration-processor-packet-utility/src/main/java/io/mosip/registration/processor/packet/utility/service/impl/PacketDecryptorImpl.java
@@ -17,7 +17,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.HttpServerErrorException;
-import org.springframework.web.client.RestTemplate;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
