c402feff3a641aec5fa26965bbac83e721905bdc
M1022006 <M1022006@A2ML18485.mindtree.com>
2020-05-09 15:50:57 +0530

    MOSIP-1040 Bio authentication stage changes

--- registration-processor/registration-processor-info-storage-service/src/main/java/io/mosip/registration/processor/packet/storage/utils/Utilities.java
+++ registration-processor/registration-processor-info-storage-service/src/main/java/io/mosip/registration/processor/packet/storage/utils/Utilities.java
@@ -203,7 +203,7 @@ public class Utilities {
 
 	private String mappingJsonString = null;
 
-	
+
 	@Autowired
 	private PacketReaderService packetReaderService;
 	
@@ -428,7 +428,7 @@ public class Utilities {
 	public JSONObject getDemographicIdentityJSONObject(String registrationId) throws IOException,
 			PacketDecryptionFailureException, io.mosip.kernel.core.exception.IOException, ApiNotAccessibleException {
 		
-	
+
 		regProcLogger.debug(LoggerFileConstant.SESSIONID.toString(), LoggerFileConstant.REGISTRATIONID.toString(),
 				registrationId, "Utilities::getDemographicIdentityJSONObject()::entry");
 		
@@ -613,7 +613,7 @@ public class Utilities {
 	 *             Signals that an I/O exception has occurred.
 	 * @throws io.mosip.registration.processor.packet.utility.exception.PacketDecryptionFailureException 
 	 */
-	public PacketMetaInfo getPacketMetaInfo(String registrationId) throws ApiNotAccessibleException, 
+	public PacketMetaInfo getPacketMetaInfo(String registrationId) throws ApiNotAccessibleException,
 			ApisResourceAccessException, io.mosip.kernel.core.exception.IOException, IOException, PacketDecryptionFailureException {
 		regProcLogger.debug(LoggerFileConstant.SESSIONID.toString(), LoggerFileConstant.REGISTRATIONID.toString(),
 				registrationId, "Utilities::getPacketMetaInfo():: entry");
