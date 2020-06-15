aa29b38611cd2dceb73ec8cf698626bcb12cd985
Monobikash Das <M1045447@mindtree.com>
2020-06-15 13:42:30 +0530

    MOSIP-820 : fixed hdfs issue

--- registration-processor/post-processor/registration-processor-registration-transaction-service/src/test/java/io/mosip/registration/processor/transaction/api/transaction/api/config/RegistrationTransactionBeanConfigTest.java
+++ registration-processor/post-processor/registration-processor-registration-transaction-service/src/test/java/io/mosip/registration/processor/transaction/api/transaction/api/config/RegistrationTransactionBeanConfigTest.java
@@ -5,7 +5,6 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
-import io.mosip.kernel.fsadapter.hdfs.util.ConnectionUtils;
 import io.mosip.registration.processor.status.validator.RegistrationStatusRequestValidator;
 import io.mosip.registration.processor.status.validator.RegistrationSyncRequestValidator;
 
@@ -13,9 +12,6 @@ import io.mosip.registration.processor.status.validator.RegistrationSyncRequestV
 @EnableWebMvc
 @ComponentScan(basePackages = { "io.mosip.registration.processor.transaction.api.*" })
 public class RegistrationTransactionBeanConfigTest {
-
-	@MockBean
-	public ConnectionUtils connectionUtil;
 
 	@MockBean
 	public RegistrationStatusRequestValidator registrationStatusRequestValidator;
