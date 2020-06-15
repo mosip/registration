bae3c07679c619db82fc276a4d682a7fa399d44b
Chukka.Sreekar@mindtree.com <M1048290@b2ml24784.mindtree.com>
2020-06-15 15:23:47 +0530

    MOSIP-1142-Refactor code

--- registration/registration-client/src/main/java/io/mosip/registration/controller/device/GuardianBiometricsController.java
+++ registration/registration-client/src/main/java/io/mosip/registration/controller/device/GuardianBiometricsController.java
@@ -2095,8 +2095,7 @@ public class GuardianBiometricsController extends BaseController /* implements I
 		LOGGER.debug("REGISTRATION - BIOMETRICS - refreshContinueButton", RegistrationConstants.APPLICATION_ID,
 				RegistrationConstants.APPLICATION_NAME, "refreshContinueButton invoked");
 
-
-		/*if (getListOfBiometricSubTypes().isEmpty()) {
+		if (getListOfBiometricSubTypes().isEmpty()) {
 
 			LOGGER.debug("REGISTRATION - BIOMETRICS - refreshContinueButton", RegistrationConstants.APPLICATION_ID,
 					RegistrationConstants.APPLICATION_NAME,
@@ -2155,8 +2154,8 @@ public class GuardianBiometricsController extends BaseController /* implements I
 				RegistrationConstants.APPLICATION_NAME, "capturedDetails >> " + capturedDetails);
 		LOGGER.debug("REGISTRATION - BIOMETRICS - refreshContinueButton", RegistrationConstants.APPLICATION_ID,
 
-		RegistrationConstants.APPLICATION_NAME, "Expression >> " + expression + " :: result >> " + result);*/
-		continueBtn.setDisable(false);
+				RegistrationConstants.APPLICATION_NAME, "Expression >> " + expression + " :: result >> " + result);
+		continueBtn.setDisable(result ? false : true);
 	}
 
 	/**
