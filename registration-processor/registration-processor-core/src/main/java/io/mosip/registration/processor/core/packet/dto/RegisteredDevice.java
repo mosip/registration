ddbcb0b69510a0be4f011e3e1884457ee7ea9347
Monobikash Das <43202165+MonobikashDas@users.noreply.github.com>
2020-07-25 17:38:06 +0530

    1.0.11 (#382)
    * MOSIP-8027 : fix device subtype issue
    
    * MOSIP-8086 : fixed audit util issue
    
    * MOSIP-8328 : device spec changes
    
    * MOSIP-8328 : device spec changes
    
    * MOSIP-8328 : reverting commit
    
    Co-authored-by: Monobikash Das <M1045447@mindtree.com>

--- registration-processor/registration-processor-core/src/main/java/io/mosip/registration/processor/core/packet/dto/RegisteredDevice.java
+++ registration-processor/registration-processor-core/src/main/java/io/mosip/registration/processor/core/packet/dto/RegisteredDevice.java
@@ -1,11 +1,17 @@
 package io.mosip.registration.processor.core.packet.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
 public class RegisteredDevice {
 
-	private String deviceCode;
-	private String deviceServiceVersion;
-	private DigitalId digitalId;
+    private String deviceCode;
+    private String deviceServiceVersion;
+    private DigitalId digitalId;
 }
