e77848f2ade479ff508b48eae46049cdb7f8178c
Monobikash Das <43202165+MonobikashDas@users.noreply.github.com>
2020-09-21 11:36:00 +0530

    Develop (#473)
    * MOSIP-8739 : set status code to processed
    
    * MOSIP-8122 : integrated new packet manager for packet-receiver & uploader
    
    * MOSIP-8122 : integrated new packet manager for validator
    
    * MOSIP-8122 : integrated new packet manager for packet-receiver & uploader
    
    * MOSIP-8122 : integrated new packet manager for validator
    
    * MOSIP-8122 : integrated new packet manager for quality checker
    
    * MOSIP-8122 : integrated new packet manager for osi
    
    * MOSIP-8122 : integrated new packet manager for bio-auth stage
    
    * MOSIP-8122 : integrated new packet manager for bio-auth stage
    
    * MOSIP-8122 : integrated new packet manager for UIN-generator stage
    
    * MOSIP-8122 : integrated new packet manager for print stage
    
    * MOSIP-8122 : integrated new packet manager for print stage
    
    * MOSIP-8122 : integrated new packet manager for print stage
    
    * MOSIP-8122 : updated test cases
    
    * MOSIP-8122 : updated errors
    
    * MOSIP-8122 : updated pom
    
    * MOSIP-8122 : updated dummy abis
    
    * MOSIP-8122 : added TODO items
    
    * MOSIP-8122 : fixed bio-dedupe server issue
    
    * MOSIP-8122 : fixed bio-dedupe server issue
    
    * MOSIP-8122 : fixed masterdata validation issue
    
    * MOSIP-8122 : added test cases
    
    * MOSIP-8122 : fixed demo dedupe issue
    
    * MOSIP-8122 : fixed demo dedupe issue
    
    * MOSIP-8122 : fixed uin generator issue
    
    * MOSIP-8122 : fixed uin generator issue
    
    * MOSIP-8122 : fixed uin generator issue
    
    * MOSIP-8122 : fixed datashare url issue
    
    * MOSIP-8122 : fixed datashare url issue
    
    * MOSIP-8122 : fixed build issue
    
    * MOSIP-8122 : fixed pom dependency issues
    
    * MOSIP-8122 : fixed test case failure
    
    * MOSIP-8122 : fixed test case failure
    
    * MOSIP-8122 : fixed abis duplicate flow
    
    * MOSIP-8122 : deleted request handler service
    
    * MOSIP-8122 : deleted request handler service
    
    * MOSIP-8122 : test cases and push trigger
    
    * MOSIP-8122 : minor issue fix
    
    * MOSIP-8567 : exception handling if send notification fails
    
    * MOSIP-8567 : object store adapter changes and auth changes
    
    * MOSIP-8567 : bugfix
    
    Co-authored-by: Monobikash Das <M1045447@mindtree.com>

--- registration-processor/pre-processor/registration-processor-packet-uploader-stage/src/main/java/io/mosip/registration/processor/packet/uploader/PacketUploaderJobApplication.java
+++ registration-processor/pre-processor/registration-processor-packet-uploader-stage/src/main/java/io/mosip/registration/processor/packet/uploader/PacketUploaderJobApplication.java
@@ -21,6 +21,7 @@ public class PacketUploaderJobApplication {
 				"io.mosip.registration.processor.packet.uploader.config",
 				"io.mosip.registration.processor.packet.manager.config",
 				"io.mosip.kernel.packetmanager.config",
+				"io.mosip.registration.processor.packet.storage.config",
 				"io.mosip.registration.processor.status.config", "io.mosip.registration.processor.rest.client.config",
 				"io.mosip.registration.processor.core.kernel.beans");
 		configApplicationContext.refresh();
