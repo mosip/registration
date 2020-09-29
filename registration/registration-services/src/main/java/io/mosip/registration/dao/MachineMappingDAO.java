bb0a9b21369d3d7a2208cd832086c1c94846f11c
M1044402 <M1044402@MT-PF10TTST>
2020-09-29 19:36:33 +0530

    MOSIP-9563 : Fixed the issue by validating device in device_master

--- registration/registration-services/src/main/java/io/mosip/registration/dao/MachineMappingDAO.java
+++ registration/registration-services/src/main/java/io/mosip/registration/dao/MachineMappingDAO.java
@@ -20,54 +20,61 @@ public interface MachineMappingDAO {
 
 	/**
 	 * This method is used to get station id by using machine name
+	 * 
 	 * @param machineName
 	 * @return
 	 * @throws RegBaseCheckedException
 	 */
 	String getStationID(String machineName) throws RegBaseCheckedException;
-	
+
 	/**
 	 * This method is used to check the validity of the device.
 	 * 
-	 * @param deviceType
-	 *            device type
-	 * @param serialNo
-	 *            serial number of the device
+	 * @param deviceType device type
+	 * @param serialNo   serial number of the device
 	 * @return It returns true when record found for the device else false
 	 */
 	boolean isValidDevice(DeviceTypes deviceType, String serialNo);
 
 	/**
-	 * This method is used to get the user mapping details that are mapped to the given machine id.
+	 * This method is used to get the user mapping details that are mapped to the
+	 * given machine id.
 	 * 
-	 * @param machineId
-	 *            machine ID
+	 * @param machineId machine ID
 	 * @return It returns the list of users against the machine
 	 */
 	List<UserMachineMapping> getUserMappingDetails(String machineId);
 
 	/**
-	 * This method is udes to fetches all the devices mapped to the registration center based on the
-	 * language code
+	 * This method is udes to fetches all the devices mapped to the registration
+	 * center based on the language code
 	 * 
-	 * @param langCode
-	 *            the language code of the device
+	 * @param langCode the language code of the device
 	 * @return All the devices mapped to registration center
 	 */
 	List<RegDeviceMaster> getDevicesMappedToRegCenter(String langCode);
 
-	/**This method is used to find whether the user exists or not
-	 * @param userId 
-	 * 			userId
+	/**
+	 * This method is used to find whether the user exists or not
+	 * 
+	 * @param userId userId
 	 * @return is exists or not
 	 */
 	boolean isExists(String userId);
-	
+
 	/**
 	 * This method is used to get the key index of the Machine based on MachineName
+	 * 
 	 * @param machineName aka host name
 	 * @return
 	 */
 	String getKeyIndexByMachineName(String machineName);
+
+	/**
+	 * This method is used to get registered device with particular device code
+	 * 
+	 * @return List of Registered device
+	 */
+	public List<RegDeviceMaster> getRegisteredDevicesBySerialNumber(String serialNumber);
 
 }
