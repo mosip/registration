bd7e72c4a52729c5a9521f6f34850f3bad407c53
M1022006 <M1022006@A2ML18485.mindtree.com>
2020-10-16 11:45:51 +0530

    print service impl test case issue fixed

--- registration-processor/registration-processor-print-service-impl/src/main/java/io/mosip/registration/processor/print/service/impl/PrintServiceImpl.java
+++ registration-processor/registration-processor-print-service-impl/src/main/java/io/mosip/registration/processor/print/service/impl/PrintServiceImpl.java
@@ -1,7 +1,5 @@
 package io.mosip.registration.processor.print.service.impl;
 
-import java.io.ByteArrayInputStream;
-import java.io.DataInputStream;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
@@ -76,6 +74,7 @@ import io.mosip.registration.processor.packet.storage.utils.Utilities;
 import io.mosip.registration.processor.print.service.exception.IDRepoResponseNull;
 import io.mosip.registration.processor.print.service.exception.PDFSignatureException;
 import io.mosip.registration.processor.print.service.exception.UINNotFoundInDatabase;
+import io.mosip.registration.processor.print.service.utility.PrintUtility;
 import io.mosip.registration.processor.rest.client.audit.builder.AuditLogRequestBuilder;
 import io.mosip.registration.processor.status.dto.InternalRegistrationStatusDto;
 import io.mosip.registration.processor.status.dto.RegistrationStatusDto;
@@ -195,6 +194,9 @@ public class PrintServiceImpl implements PrintService<Map<String, byte[]>> {
 
 	@Autowired
 	private DigitalSignatureUtility digitalSignatureUtility;
+
+	@Autowired
+	private PrintUtility printUtility;
 
 	/*
 	 * (non-Javadoc)
@@ -561,7 +563,7 @@ public class PrintServiceImpl implements PrintService<Map<String, byte[]>> {
 			List<String> subtype = new ArrayList<>();
 			byte[] photoByte = util.getImageBytes(value, FACE, subtype);
 			if (photoByte != null) {
-				String data = Base64.getEncoder().encodeToString(extractFaceImageData(photoByte));
+				String data = Base64.getEncoder().encodeToString(printUtility.extractFaceImageData(photoByte));
 				attributes.put(APPLICANT_PHOTO, "data:image/png;base64," + data);
 				isPhotoSet = true;
 			}
@@ -775,52 +777,6 @@ public class PrintServiceImpl implements PrintService<Map<String, byte[]>> {
 		return parameter;
 	}
 
-	public byte[] extractFaceImageData(byte[] decodedBioValue) {
 
-		try (DataInputStream din = new DataInputStream(new ByteArrayInputStream(decodedBioValue))) {
-
-			byte[] format = new byte[4];
-			din.read(format, 0, 4);
-			byte[] version = new byte[4];
-			din.read(version, 0, 4);
-			int recordLength = din.readInt();
-			short numberofRepresentionRecord = din.readShort();
-			byte certificationFlag = din.readByte();
-			byte[] temporalSequence = new byte[2];
-			din.read(temporalSequence, 0, 2);
-			int representationLength = din.readInt();
-			byte[] representationData = new byte[representationLength - 4];
-			din.read(representationData, 0, representationData.length);
-			try (DataInputStream rdin = new DataInputStream(new ByteArrayInputStream(representationData))) {
-				byte[] captureDetails = new byte[14];
-				rdin.read(captureDetails, 0, 14);
-				byte noOfQualityBlocks = rdin.readByte();
-				if (noOfQualityBlocks > 0) {
-					byte[] qualityBlocks = new byte[noOfQualityBlocks * 5];
-					rdin.read(qualityBlocks, 0, qualityBlocks.length);
-				}
-				short noOfLandmarkPoints = rdin.readShort();
-				byte[] facialInformation = new byte[15];
-				rdin.read(facialInformation, 0, 15);
-				if (noOfLandmarkPoints > 0) {
-					byte[] landmarkPoints = new byte[noOfLandmarkPoints * 8];
-					rdin.read(landmarkPoints, 0, landmarkPoints.length);
-				}
-				byte faceType = rdin.readByte();
-				byte imageDataType = rdin.readByte();
-				byte[] otherImageInformation = new byte[9];
-				rdin.read(otherImageInformation, 0, otherImageInformation.length);
-				int lengthOfImageData = rdin.readInt();
-
-				byte[] image = new byte[lengthOfImageData];
-				rdin.read(image, 0, lengthOfImageData);
-
-				return image;
-			}
-		} catch (Exception ex) {
-			throw new PDFGeneratorException(PDFGeneratorExceptionCodeConstant.PDF_EXCEPTION.getErrorCode(),
-					ex.getMessage() + ExceptionUtils.getStackTrace(ex));
-		}
-	}
 
 }
