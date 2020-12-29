7a3f440474348bcb562fdb45bf54e1fea331f57d
sadanandegowda <43214830+sadanandegowda@users.noreply.github.com>
2020-12-29 12:02:03 +0530

    Updated manual verification table as per the new enhancements (#650)

--- db_release_scripts/mosip_regprc/sql/1.1.4_regprc-scripts_revoke.sql
+++ db_release_scripts/mosip_regprc/sql/1.1.4_regprc-scripts_revoke.sql
@@ -1,15 +1,19 @@
 -- -------------------------------------------------------------------------------------------------
 -- Database Name: mosip_regprc
--- Release Version 	: 1.0.9
+-- Release Version 	: 1.1.4
 -- Purpose    		: Revoking Database Alter deployement done for release in Registration ProcessorDB.       
 -- Create By   		: Sadanandegowda DM
--- Created Date		: 27-Apr-2020
+-- Created Date		: Dec-2020
 -- 
 -- Modified Date        Modified By         Comments / Remarks
 -- -------------------------------------------------------------------------------------------------
 
 \c mosip_regprc sysadmin
 
-ALTER TABLE regprc.abis_response_det ADD COLUMN score numeric(6,3) NOT NULL;
+ALTER TABLE regprc.reg_manual_verification DROP COLUMN IF EXISTS ref_regtrn_id;
+
+ALTER TABLE regprc.reg_manual_verification DROP COLUMN IF EXISTS request_id;
+
+ALTER TABLE regprc.reg_manual_verification DROP COLUMN IF EXISTS res_text;
 
 ----------------------------------------------------------------------------------------------------
