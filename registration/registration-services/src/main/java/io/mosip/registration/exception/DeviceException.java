b1f0e04da40fb0ef714fa05cb3fc7374a66cdb4e
ase-101 <>
2021-01-22 04:16:34 +0530

    Added JWT signature verification for every MDS response and digitalId

---
+++ registration/registration-services/src/main/java/io/mosip/registration/exception/DeviceException.java
@@ -0,0 +1,56 @@
+package io.mosip.registration.exception;
+
+import io.mosip.kernel.core.exception.BaseCheckedException;
+import io.mosip.kernel.core.exception.ExceptionUtils;
+import io.mosip.kernel.core.logger.spi.Logger;
+import io.mosip.registration.config.AppConfig;
+
+import static io.mosip.registration.constants.RegistrationConstants.APPLICATION_ID;
+import static io.mosip.registration.constants.RegistrationConstants.APPLICATION_NAME;
+
+public class DeviceException extends BaseCheckedException  {
+
+    /**
+     * Instance of {@link Logger}
+     */
+    private static final Logger LOGGER = AppConfig.getLogger(RegBaseCheckedException.class);
+
+    /**
+     * Constructs a new checked exception
+     */
+    public DeviceException() {
+        super();
+    }
+
+    /**
+     * Constructs a new checked exception with the specified detail message and
+     * error code.
+     *
+     * @param errorCode
+     *            the error code
+     * @param errorMessage
+     *            the detail message.
+     */
+    public DeviceException(String errorCode, String errorMessage) {
+        super(errorCode, errorMessage);
+        LOGGER.error("REGISTRATION - CHECKED_EXCEPTION", APPLICATION_NAME,
+                APPLICATION_ID, errorCode + "-->" + errorMessage);
+    }
+
+    /**
+     * Constructs a new checked exception with the specified detail message and
+     * error code.
+     *
+     * @param errorCode
+     *            the error code
+     * @param errorMessage
+     *            the detail message
+     * @param throwable
+     *            the specified cause
+     */
+    public DeviceException(String errorCode, String errorMessage, Throwable throwable) {
+        super(errorCode, errorMessage, throwable);
+        LOGGER.error("REGISTRATION - CHECKED_EXCEPTION", APPLICATION_NAME, APPLICATION_ID,
+                errorCode + "-->" + errorMessage + "-->" + ExceptionUtils.getStackTrace(throwable));
+    }
+}
