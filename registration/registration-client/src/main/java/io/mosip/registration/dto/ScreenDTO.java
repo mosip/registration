2dfc16d1aba01eb0683722197ad3b1fd48d08eb8
M1044402 <M1044402@MT-PF10TTST>
2021-02-12 18:18:21 +0530

    MOSIP-10268 : Added Page Flow

---
+++ registration/registration-client/src/main/java/io/mosip/registration/dto/ScreenDTO.java
@@ -0,0 +1,20 @@
+package io.mosip.registration.dto;
+
+import lombok.Data;
+
+import javafx.scene.Node;
+import lombok.Data;
+
+import javafx.scene.Node;
+import lombok.Data;
+
+import javafx.scene.Node;
+import lombok.Data;
+
+@Data
+public class ScreenDTO {
+
+	private String screenName;
+	private String screenDesc;
+	private Node screenNode;
+}
