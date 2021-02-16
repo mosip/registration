42ce302123d29fd1fc41bb6600e6e5e40702fcf8
rambhatt1591 <ram.bhatt@technoforte.co.in>
2021-02-17 05:17:02 +0530

    MOSIP 11399 Make all deleted flags not null and default false

--- db_scripts/mosip_reg/ddl/reg-key_store_DRBY.sql
+++ db_scripts/mosip_reg/ddl/reg-key_store_DRBY.sql
@@ -8,7 +8,7 @@
 -- 
 -- Modified Date        Modified By         Comments / Remarks
 -- ------------------------------------------------------------------------------------------
--- 
+-- Jan-2021		Ram Bhatt	    Set is_deleted flag to not null and default false
 -- ------------------------------------------------------------------------------------------
 
 -- object: reg.key_store | type: TABLE --
@@ -25,7 +25,7 @@ CREATE TABLE reg.key_store(
 	cr_dtimes timestamp NOT NULL,
 	upd_by character varying(256),
 	upd_dtimes timestamp,
-	is_deleted boolean,
+	is_deleted boolean NOT NULL DEFAULT FALSE,
 	del_dtimes timestamp,
 	CONSTRAINT pk_keystr_id PRIMARY KEY (id)
 
--- db_scripts/mosip_reg/ddl/reg-reg_center_machine_device.sql
+++ db_scripts/mosip_reg/ddl/reg-reg_center_machine_device.sql
@@ -8,7 +8,7 @@
 -- 
 -- Modified Date        Modified By         Comments / Remarks
 -- ------------------------------------------------------------------------------------------
--- 
+-- Jan-2021		Ram Bhatt	    Set is_deleted flag to not null and default false
 -- ------------------------------------------------------------------------------------------
 
 -- NOTE: the code below contains the SQL for the selected object
@@ -33,7 +33,7 @@ CREATE TABLE reg.reg_center_machine_device(
 	cr_dtimes timestamp NOT NULL,
 	upd_by character varying(256),
 	upd_dtimes timestamp,
-	is_deleted boolean,
+	is_deleted boolean NOT NULL DEFAULT FALSE,
 	del_dtimes timestamp,
 	CONSTRAINT pk_cntrmdev_cntr_id PRIMARY KEY (regcntr_id,machine_id,device_id)
 
