0dad2a535e359fd59211a8e028c26b03f3051d47
Loganathan Sekar <Loganathan.Sekar@mindtree.com>
2021-03-02 08:12:38 +0530

    Loading stage info from config

--- registration-processor/registration-processor-stages-executor/src/main/java/io/mosip/registration/processor/stages/executor/ExecuteStagesApplication.java
+++ registration-processor/registration-processor-stages-executor/src/main/java/io/mosip/registration/processor/stages/executor/ExecuteStagesApplication.java
@@ -1,37 +1,32 @@
 package io.mosip.registration.processor.stages.executor;
 
-import java.util.Arrays;
 import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
+import org.springframework.beans.BeansException;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import io.mosip.registration.processor.core.abstractverticle.MosipVerticleAPIManager;
 import io.mosip.registration.processor.core.spi.stage.StageInfo;
+import io.mosip.registration.processor.stages.executor.config.StagesConfig;
 
 /**
  * External Stage application
  *
  */
 public class ExecuteStagesApplication {
-
-	private static final String PROP_STAGE_INFO_BASE_PACKAGES_DEFAULT_VALUE = "io.mosip.registration.processor.**.stageinfo";
-	private static final String PROP_STAGE_INFO_BASE_PACKAGES_KEY = "stage.info.base.packages";
-
+	
 	/**
 	 * main method to launch external stage application
 	 * 
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		try (AnnotationConfigApplicationContext stageInfoApplicationContext = new AnnotationConfigApplicationContext();) {
-			stageInfoApplicationContext.scan(getBaseStageInfoPackages());
-			stageInfoApplicationContext.refresh();
-			
-			Map<String, StageInfo> allStageInfos = stageInfoApplicationContext.getBeansOfType(StageInfo.class);
-
-			List<StageInfo> stageInfos = allStageInfos.values().stream().collect(Collectors.toList());
+		try (AnnotationConfigApplicationContext stageInfoApplicationContext = new AnnotationConfigApplicationContext(StagesConfig.class);) {
+			StagesConfig stagesConfig = stageInfoApplicationContext.getBean(StagesConfig.class);
+			List<StageInfo> stageInfos = stagesConfig.getStages();
 			try (AnnotationConfigApplicationContext mainApplicationContext = new AnnotationConfigApplicationContext();) {
 				// Scan packages
 				stageInfos.stream().forEach(stageInfo -> {
@@ -41,19 +36,42 @@ public class ExecuteStagesApplication {
 				// Refresh the context
 				mainApplicationContext.refresh();
 
-				stageInfos.forEach(stageInfo -> {
-					MosipVerticleAPIManager stageBean = mainApplicationContext.getBean(stageInfo.getStageBeanClass());
-					stageBean.deployVerticle();
-				});
+				ExecutorService executorService = Executors.newFixedThreadPool(stageInfos.size());
+				stageInfos.forEach(stageInfo -> executorService.execute(() -> {
+					try {
+						MosipVerticleAPIManager stageBean = getStageBean(mainApplicationContext, stageInfo);
+						stageBean.deployVerticle();
+					} catch (Exception e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}));
+				try {
+					executorService.awaitTermination(10000, TimeUnit.DAYS);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				executorService.shutdown();
 			}
 		}
 
 	}
 
-	private static String[] getBaseStageInfoPackages() {
-		String pkgs = System.getProperty(PROP_STAGE_INFO_BASE_PACKAGES_KEY, PROP_STAGE_INFO_BASE_PACKAGES_DEFAULT_VALUE);
-		return Arrays.stream(pkgs.split(",")).filter(str -> !str.isEmpty())
-					.toArray(size -> new String[size]);
+	private static MosipVerticleAPIManager getStageBean(AnnotationConfigApplicationContext mainApplicationContext,
+			StageInfo stageInfo) throws Exception {
+		try {
+			Class<?> stageBeanClass = Class.forName(stageInfo.getStageClass());
+			if(MosipVerticleAPIManager.class.isAssignableFrom(stageBeanClass)) {
+				Object bean = mainApplicationContext.getBean(stageBeanClass);
+				MosipVerticleAPIManager stageBean = (MosipVerticleAPIManager) bean;
+				return stageBean;
+			} else {
+				//TODO throw invalid config exception
+				throw new Exception("Invalid config");
+			}
+		} catch (BeansException | ClassNotFoundException e) {
+			throw e;
+		}
 	}
-
 }
---
+++ registration-processor/registration-processor-stages-executor/src/main/java/io/mosip/registration/processor/stages/executor/config/StagesConfig.java
@@ -0,0 +1,25 @@
+package io.mosip.registration.processor.stages.executor.config;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+import io.mosip.registration.processor.core.spi.stage.StageInfo;
+
+@Configuration
+@EnableConfigurationProperties
+@PropertySource(value = "classpath:application.properties")
+@ConfigurationProperties(prefix = "mosip.regproc.stageinfo")
+public class StagesConfig {
+	
+	private List<StageInfo> stages = new ArrayList<>();
+	
+	public List<StageInfo> getStages() {
+		return stages;
+	}
+	
+}
