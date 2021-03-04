d2a898e033f717512d5e35acb55e9a9590504c29
Monobikash Das <43202165+MonobikashDas@users.noreply.github.com>
2021-03-04 13:42:15 +0530

    MOSIP-12124 : initial stage structure and securezone-notification to library (#763)
    * MOSIP-11842: Fixed user role issue
    
    * MOSIP-11305: Fixed exception image issue in preview
    
    * Fixed placeholder injection issue
    
    * MOSIP-11244 : Time module registered with default object mapper bean (#741)
    
    * MOSIP-10268: Modified header section to display screen names
    
    * Changed node orientation for arabic fields
    
    * Modified document value
    
    * MOSIP-10268 : Fixed alignment issues
    
    * MOSIP-10268: Added grouping of fields
    
    * UI Changes
    
    * Removed unused classes, Center remap issue fix
    
    * fixed compile issues in test cases
    
    * Fixed NPE with single language
    
    * MDM trust validation based on flag
    
    * MOSIP-12124 : changed pom version
    
    * MOSIP-12124 : initial stage structure
    
    Co-authored-by: HimajaDhanyamraju2 <dhanyamraju.himaja2@mindtree.com>
    Co-authored-by: M1044402 <M1044402@MT-PF10TTST>
    Co-authored-by: Vishwa <visu.vs1@gmail.com>
    Co-authored-by: ase-101 <sunkadaeanusha@gmail.com>
    Co-authored-by: HimajaDhanyamraju2 <43470317+HimajaDhanyamraju2@users.noreply.github.com>
    Co-authored-by: ase-101 <>
    Co-authored-by: Sasikumar Ganesan <gsasikumar@gmail.com>
    Co-authored-by: Monobikash Das <M1045447@mindtree.com>

--- registration-processor/pre-processor/registration-processor-securezone-notification-stage/src/main/java/io/mosip/registration/processor/securezone/notification/stage/SecurezoneNotificationStage.java
+++ registration-processor/pre-processor/registration-processor-securezone-notification-stage/src/main/java/io/mosip/registration/processor/securezone/notification/stage/SecurezoneNotificationStage.java
@@ -33,9 +33,19 @@ import io.vertx.ext.web.RoutingContext;
 import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 
 @Component
+@Configuration
+@ComponentScan(basePackages = {
+        "io.mosip.registration.processor.core.config",
+        "io.mosip.registration.processor.securezone.notification.config",
+        "io.mosip.registration.processor.packet.manager.config",
+        "io.mosip.registration.processor.status.config", "io.mosip.registration.processor.rest.client.config",
+        "io.mosip.registration.processor.core.kernel.beans"
+})
 public class SecurezoneNotificationStage extends MosipVerticleAPIManager {
 
     /**
