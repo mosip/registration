4a39bc1559499deca08b75e4fbe8e6e2525ad7c5
Loganathan Sekar <Loganathan.Sekar@mindtree.com>
2021-03-09 17:48:28 +0530

    Load config from folder

--- registration-processor/mosip-stage-executor/src/main/java/io/mosip/registration/processor/stages/executor/config/StagesConfig.java
+++ registration-processor/mosip-stage-executor/src/main/java/io/mosip/registration/processor/stages/executor/config/StagesConfig.java
@@ -12,7 +12,9 @@ import org.springframework.context.annotation.PropertySource;
 
 @Configuration
 @RefreshScope
+//This is added to fix issue with loading beans with @RefreshScope in the stages
 @ImportAutoConfiguration({RefreshAutoConfiguration.class})
+//This is added to fix issue with loading stage specific properties from bootstrap, expected by the stages config.
 @PropertySource("classpath:bootstrap.properties")
 public class StagesConfig {
 	
@@ -20,9 +22,15 @@ public class StagesConfig {
 	@Value("${mosip.regproc.stageClasses}")
 	private String[] stageClasses;
 	
+	@Value("${config.folder}")
+	private String configFolder;
 	
 	public List<String> getStageClasses() {
 		return Arrays.asList(stageClasses);
+	}
+	
+	public String getConfigFolder() {
+		return configFolder;
 	}
 	
 }
