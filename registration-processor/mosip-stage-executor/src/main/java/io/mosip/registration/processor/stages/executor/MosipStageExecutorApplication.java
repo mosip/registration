e14e91b89719c457c85c82ff3a0a9cc5b89fe276
Loganathan Sekar <Loganathan.Sekar@mindtree.com>
2021-03-09 17:48:28 +0530

    Load config from folder

--- registration-processor/mosip-stage-executor/src/main/java/io/mosip/registration/processor/stages/executor/MosipStageExecutorApplication.java
+++ registration-processor/mosip-stage-executor/src/main/java/io/mosip/registration/processor/stages/executor/MosipStageExecutorApplication.java
@@ -1,18 +1,27 @@
 package io.mosip.registration.processor.stages.executor;
 
-import java.util.LinkedList;
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
+import java.util.Objects;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.core.env.MutablePropertySources;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.support.ResourcePropertySource;
 
 import io.mosip.kernel.core.exception.ExceptionUtils;
 import io.mosip.registration.processor.core.abstractverticle.MosipVerticleAPIManager;
+import io.mosip.registration.processor.stages.executor.config.CustomEnvironment;
 import io.mosip.registration.processor.stages.executor.config.StagesConfig;
 
 /**
@@ -30,19 +39,35 @@ public class MosipStageExecutorApplication {
 	 */
 	public static void main(String[] args) {
 		regProcLogger.info("Starting mosip-stage-executor>>>>>>>>>>>>>>");
-		try (AnnotationConfigApplicationContext stageInfoApplicationContext = new AnnotationConfigApplicationContext(StagesConfig.class);) {
+		try (AnnotationConfigApplicationContext stageInfoApplicationContext = new AnnotationConfigApplicationContext(
+				StagesConfig.class);) {
 			StagesConfig stagesConfig = stageInfoApplicationContext.getBean(StagesConfig.class);
-			List<String> stageClasses = stagesConfig.getStageClasses();
-			try (AnnotationConfigApplicationContext mainApplicationContext = new AnnotationConfigApplicationContext();) {
-				List<String> basePackages = stageClasses.stream()
-												.map(classStr -> getPackageOfClass(classStr))
-												.collect(Collectors.toCollection(() -> new LinkedList<>()));
-				basePackages.add(0, getPackageOfClass(StagesConfig.class.getCanonicalName()));
-				mainApplicationContext.scan(basePackages.toArray(size -> new String[size]));
+			List<Class<MosipVerticleAPIManager>> stageClasses = stagesConfig.getStageClasses().stream()
+					.map(classStr -> {
+						try {
+							return (Class<MosipVerticleAPIManager>) Class.forName(classStr);
+						} catch (ClassNotFoundException e1) {
+							regProcLogger.error("Unable to load Bean : " + classStr);
+							throw new RuntimeException("Invalid config");
+						}
+					}).filter(Objects::nonNull).collect(Collectors.toList());
+			Class<?>[] entrypointConfigClasses = Stream
+					.concat(Stream.of(StagesConfig.class), stageClasses.stream())
+					.toArray(size -> new Class<?>[size]);
+			final String configFolder = stagesConfig.getConfigFolder();
+			try (AnnotationConfigApplicationContext mainApplicationContext = new AnnotationConfigApplicationContext(
+					entrypointConfigClasses) {
+				
+				protected org.springframework.core.env.ConfigurableEnvironment createEnvironment() {
+					ConfigurableEnvironment environment = super.createEnvironment();
+					environment.merge(createCustomEnvironment(configFolder));
+					return environment;
+				}
 
-				// Refresh the context
-				mainApplicationContext.refresh();
-
+				protected void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) {
+					System.out.println(beanFactory);
+				}
+			};) {
 				if (!stageClasses.isEmpty()) {
 					ExecutorService executorService = Executors.newFixedThreadPool(stageClasses.size());
 					stageClasses.forEach(stageClass -> executorService.execute(() -> {
@@ -50,8 +75,8 @@ public class MosipStageExecutorApplication {
 							MosipVerticleAPIManager stageBean = getStageBean(mainApplicationContext, stageClass);
 							stageBean.deployVerticle();
 						} catch (Exception e) {
-							regProcLogger.error("Exception occured while loading verticles. " +
-									"Please make sure correct verticle name was passed from deployment script.",
+							regProcLogger.error("Exception occured while loading verticles. "
+									+ "Please make sure correct verticle name was passed from deployment script.",
 									ExceptionUtils.getStackTrace(e));
 						}
 					}));
@@ -61,22 +86,41 @@ public class MosipStageExecutorApplication {
 		}
 
 	}
+	
+	public static CustomEnvironment createCustomEnvironment(String configFolder) {
+		CustomEnvironment newEnv = new CustomEnvironment();
+		MutablePropertySources propSources = new MutablePropertySources();
+		
+		FileSystemResource[] fileSystemResource = new FileSystemResource[] {
+				new FileSystemResource(new File(configFolder + "/bootstrap.properties")), new FileSystemResource(
+						new File(configFolder + "/registration-processor-packet-classifier-stage.properties")) };
+		Stream.of(fileSystemResource)
+				.forEach(res -> {
+					try {
+						propSources.addFirst(new ResourcePropertySource(res));
+					} catch (IOException e) {
+						throw new RuntimeException("Unable to load config");
+					}
+				});
+		
+		newEnv.customizePropertySources(propSources);
+		return newEnv;
+	}
 
 	private static String getPackageOfClass(String classStr) {
 		return classStr.substring(0, classStr.lastIndexOf('.'));
 	}
 
 	private static MosipVerticleAPIManager getStageBean(AnnotationConfigApplicationContext mainApplicationContext,
-			String stageClass) throws Exception {
+			Class<?> stageBeanClass) throws Exception {
 		try {
-			Class<?> stageBeanClass = Class.forName(stageClass);
 			if (MosipVerticleAPIManager.class.isAssignableFrom(stageBeanClass)) {
 				Object bean = mainApplicationContext.getBean(stageBeanClass);
 				MosipVerticleAPIManager stageBean = (MosipVerticleAPIManager) bean;
-				regProcLogger.info("Successfully loaded Bean : " + stageClass);
+				regProcLogger.info("Successfully loaded Bean : " + stageBeanClass.getCanonicalName());
 				return stageBean;
 			} else {
-				regProcLogger.error("Unable to load Bean : " + stageClass);
+				regProcLogger.error("Unable to load Bean : " + stageBeanClass.getCanonicalName());
 				throw new Exception("Invalid config");
 			}
 		} catch (BeansException | ClassNotFoundException e) {
