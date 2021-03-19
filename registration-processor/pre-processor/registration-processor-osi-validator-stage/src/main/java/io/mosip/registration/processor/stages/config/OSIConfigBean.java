31dae508f0e0b3aaa8c5d278f26be911f36886ff
Manoj SP <Manoj.SP@mindtree.com>
2021-03-19 10:52:10 +0530

    [MOSIP-12124] converted stages to lib

--- registration-processor/pre-processor/registration-processor-osi-validator-stage/src/main/java/io/mosip/registration/processor/stages/config/OSIConfigBean.java
+++ registration-processor/pre-processor/registration-processor-osi-validator-stage/src/main/java/io/mosip/registration/processor/stages/config/OSIConfigBean.java
@@ -13,29 +13,10 @@ import io.mosip.kernel.core.crypto.spi.CryptoCoreSpec;
 import io.mosip.kernel.crypto.jce.core.CryptoCore;
 import io.mosip.kernel.keygenerator.bouncycastle.KeyGenerator;
 import io.mosip.registration.processor.packet.storage.utils.AuthUtil;
-
-import io.mosip.registration.processor.stages.osivalidator.OSIValidator;
-import io.mosip.registration.processor.stages.osivalidator.OSIValidatorStage;
-import io.mosip.registration.processor.stages.osivalidator.UMCValidator;
 import io.mosip.registration.processor.stages.osivalidator.utils.OSIUtils;
 
 @Configuration
 public class OSIConfigBean {
-
-	@Bean
-	public OSIValidator getOSIValidator() {
-		return new OSIValidator();
-	}
-
-	@Bean
-	public UMCValidator getUMCValidator() {
-		return new UMCValidator();
-	}
-
-	@Bean
-	public OSIValidatorStage getOSIValidatorStage() {
-		return new OSIValidatorStage();
-	}
 
 	@Bean
 	public OSIUtils getOSIUtils() {
