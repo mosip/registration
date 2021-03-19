30730546b6496d96de5e1221a37f39c823bb5b67
urviljoshi <urvvil08@gmail.com>
2021-03-19 21:21:19 +0530

    predicate modified

--- registration-processor/registration-processor-common-camel-bridge/src/main/java/io/mosip/registration/processor/camel/bridge/intercepter/RoutePredicate.java
+++ registration-processor/registration-processor-common-camel-bridge/src/main/java/io/mosip/registration/processor/camel/bridge/intercepter/RoutePredicate.java
@@ -3,13 +3,8 @@ package io.mosip.registration.processor.camel.bridge.intercepter;
 import java.io.IOException;
 import java.util.regex.Pattern;
 
-import javax.annotation.PostConstruct;
-
 import org.apache.camel.Exchange;
 import org.apache.camel.Predicate;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Component;
 
 import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.core.JsonProcessingException;
@@ -31,10 +26,7 @@ public class RoutePredicate implements Predicate {
 
 	private static final Logger LOGGER = RegProcessorLogger.getLogger(RouteIntercepter.class);
 
-	//@Value("${mosip.regproc.camelbridge.pause-settings}")
-	private String settingsString;
 
-	//@Value("${mosip.regproc.camelbridge.intercept-hotlisted-key}")
 	private String hotlistedTagKey;
 
 	
@@ -43,25 +35,21 @@ public class RoutePredicate implements Predicate {
 	Setting[] settings = null;
 	
 	public RoutePredicate(final String settingsString,final String hotlistedTagKey) {
-		this.settingsString=settingsString;
 		this.hotlistedTagKey=hotlistedTagKey;
 		objectMapper= new ObjectMapper();
 		try {
 			settings = objectMapper.readValue(settingsString, Setting[].class);
 		} catch (JsonParseException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			LOGGER.error(LoggerFileConstant.SESSIONID.toString(), LoggerFileConstant.USERID.toString(), "",
+					"RoutePredicate::exception " + e.getMessage());
 		} catch (JsonMappingException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			LOGGER.error(LoggerFileConstant.SESSIONID.toString(), LoggerFileConstant.USERID.toString(), "",
+					"RoutePredicate::exception " + e.getMessage());
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			LOGGER.error(LoggerFileConstant.SESSIONID.toString(), LoggerFileConstant.USERID.toString(), "",
+					"RoutePredicate::exception " + e.getMessage());
 		}
-		for(Setting s:settings) {
-			System.out.println("s.getFromAddress() " + s.getFromAddress());
-			System.out.println("s.getHotlistedReason() " + s.getHotlistedReason());
-		}
+		
 	}
 
 	
@@ -72,12 +60,6 @@ public class RoutePredicate implements Predicate {
 		String message = (String) exchange.getMessage().getBody();
 		JsonObject json = new JsonObject(message);
 		JsonObject tags = json.getJsonObject("tags");
-		LOGGER.info(LoggerFileConstant.SESSIONID.toString(), LoggerFileConstant.USERID.toString(), "",
-				"exchange.getFromEndpoint().toString() " + exchange.getFromEndpoint().toString());
-		LOGGER.info(LoggerFileConstant.SESSIONID.toString(), LoggerFileConstant.USERID.toString(), "",
-				" tags.getString(hotlistedTagKey) " +  tags.getString(hotlistedTagKey));
-		LOGGER.info(LoggerFileConstant.SESSIONID.toString(), LoggerFileConstant.USERID.toString(), "",
-				" tags.containsKey(hotlistedTagKey) " +  tags.containsKey(hotlistedTagKey));
 		if (tags.containsKey(hotlistedTagKey)) {
 			String fromAddress = exchange.getFromEndpoint().toString();
 			for (Setting setting : settings) {
@@ -95,7 +77,7 @@ public class RoutePredicate implements Predicate {
 						exchange.getMessage().setBody(objectMapper.writeValueAsString(workflowEventDTO));
 					} catch (JsonProcessingException e) {
 						LOGGER.error(LoggerFileConstant.SESSIONID.toString(), LoggerFileConstant.USERID.toString(), "",
-								"RouteIntercepter::intercept()::exception " + e.getMessage());
+								"RoutePredicate::matches()::exception " + e.getMessage());
 					}
 					break;
 				} else {
