1ed2b5516f6f12be6337880fbb4d892e9bb25a06
Loganathan Sekar <Loganathan.Sekar@mindtree.com>
2021-04-26 16:20:33 +0530

    Kept only necessary packages

--- registration-processor/pre-processor/registration-processor-securezone-notification-stage/src/main/java/io/mosip/registration/processor/securezone/notification/stage/SecurezoneNotificationStage.java
+++ registration-processor/pre-processor/registration-processor-securezone-notification-stage/src/main/java/io/mosip/registration/processor/securezone/notification/stage/SecurezoneNotificationStage.java
@@ -41,18 +41,10 @@ import org.springframework.stereotype.Component;
 @Configuration
 @ComponentScan(basePackages = {
 		"io.mosip.registration.processor.core.config",
-		"io.mosip.registration.processor.stages.config", 
-		"io.mosip.registrationprocessor.stages.config", 
-		"io.mosip.registration.processor.status.config",
-		"io.mosip.registration.processor.rest.client.config", 
-		"io.mosip.registration.processor.packet.storage.config",
-		"io.mosip.registration.processor.packet.manager.config", 
-		"io.mosip.kernel.idobjectvalidator.config",
-		"io.mosip.registration.processor.core.kernel.beans",
-        "io.mosip.registration.processor.core.config",
         "io.mosip.registration.processor.securezone.notification.config",
         "io.mosip.registration.processor.packet.manager.config",
-        "io.mosip.registration.processor.status.config", "io.mosip.registration.processor.rest.client.config",
+        "io.mosip.registration.processor.status.config", 
+        "io.mosip.registration.processor.rest.client.config",
         "io.mosip.registration.processor.core.kernel.beans"
 })
 public class SecurezoneNotificationStage extends MosipVerticleAPIManager {
