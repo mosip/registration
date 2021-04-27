2eb8dad3ddfa783f01965385007b6f1ba9745c77
rambhatt1591 <ram.bhatt@technoforte.co.in>
2021-04-27 10:09:20 +0530

    MOSIP-12860 resume_remove_tags column added to registration table

--- db_release_scripts/mosip_regprc/sql/1.2_regprc-scripts_release.sql
+++ db_release_scripts/mosip_regprc/sql/1.2_regprc-scripts_release.sql
@@ -7,10 +7,15 @@
 -- 
 -- Modified Date        Modified By         Comments / Remarks
 -- -------------------------------------------------------------------------------------------------
-
+-- Apr-2021		Ram Bhatt	   Added resume_remove_tags column to registration table
+----------------------------------------------------------------------------------------------------
 \c mosip_regprc sysadmin
 
 ALTER TABLE reg_prc.registration ADD COLUMN resume_timestamp timestamp;
 ALTER TABLE reg_prc.registration ADD COLUMN default_resume_action character varying(50);
+
+---------------------------------------------------------------------------------------------------
+
+ALTER TABLE reg_prc.registration ADD COLUMN resume_remove_tags character varying(256);
 
 ----------------------------------------------------------------------------------------------------
