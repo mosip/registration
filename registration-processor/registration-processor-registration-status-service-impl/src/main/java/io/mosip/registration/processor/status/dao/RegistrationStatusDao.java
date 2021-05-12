d2dca5ded92c0e30e0a00295ea0e91a8d1dc033c
Monobikash Das <43202165+MonobikashDas@users.noreply.github.com>
2021-05-12 20:04:09 +0530

    MOSIP-13229 : index with latest_trn_dtimes to make the query faster (#940)
    Co-authored-by: Monobikash Das <M1045447@mindtree.com>

--- registration-processor/registration-processor-registration-status-service-impl/src/main/java/io/mosip/registration/processor/status/dao/RegistrationStatusDao.java
+++ registration-processor/registration-processor-registration-status-service-impl/src/main/java/io/mosip/registration/processor/status/dao/RegistrationStatusDao.java
@@ -177,12 +177,12 @@ public class RegistrationStatusDao {
 		Map<String, Object> params = new HashMap<>();
 		String className = RegistrationStatusEntity.class.getSimpleName();
 		String alias = RegistrationStatusEntity.class.getName().toLowerCase().substring(0, 1);
-		LocalDateTime timeDifference = LocalDateTime.now().minusSeconds(elapseTime);
+		LocalDateTime timeDifference = DateUtils.getUTCCurrentDateTime().minusSeconds(elapseTime);
 
-		String queryStr = SELECT_DISTINCT + alias + FROM + className + EMPTY_STRING + alias + WHERE + alias
-				+ ".latestTransactionStatusCode IN :status" + EMPTY_STRING + AND + EMPTY_STRING + alias
-				+ ".regProcessRetryCount<=" + ":reprocessCount" + EMPTY_STRING + AND + EMPTY_STRING + alias
-				+ ".latestTransactionTimes<" + ":timeDifference";
+		String queryStr = SELECT_DISTINCT + alias + FROM + className + EMPTY_STRING + alias + WHERE + EMPTY_STRING + alias
+				+ ".latestTransactionTimes<" + ":timeDifference " + AND + EMPTY_STRING + alias
+				+ ".latestTransactionStatusCode IN :status " + AND + EMPTY_STRING + alias
+				+ ".regProcessRetryCount<=" + ":reprocessCount order by "+ alias + ".updateDateTime asc";
 
 		params.put("status", status);
 		params.put("reprocessCount", reprocessCount);
@@ -196,7 +196,7 @@ public class RegistrationStatusDao {
 		Map<String, Object> params = new HashMap<>();
 		String className = RegistrationStatusEntity.class.getSimpleName();
 		String alias = RegistrationStatusEntity.class.getName().toLowerCase().substring(0, 1);
-		LocalDateTime timeDifference = LocalDateTime.now().minusSeconds(elapseTime);
+		LocalDateTime timeDifference = DateUtils.getUTCCurrentDateTime().minusSeconds(elapseTime);
 
 		String queryStr = SELECT_DISTINCT + alias + FROM + className + EMPTY_STRING + alias + WHERE + alias
 				+ ".latestTransactionStatusCode IN :status" + EMPTY_STRING + AND + EMPTY_STRING + alias
