c237cd1102b4ec2da108a37de8f3268ed3074af2
SatishG_Neosoft <NEOSOFT_SatishG@nsdl.co.in>
2021-05-19 16:18:45 +0530

    MOSIP-13118 resolved config related issues

--- registration-processor/pre-processor/registration-processor-operator-validator-stage/src/main/java/io/mosip/registration/processor/stages/config/OperatorValidatorConfig.java
+++ registration-processor/pre-processor/registration-processor-operator-validator-stage/src/main/java/io/mosip/registration/processor/stages/config/OperatorValidatorConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 
 import io.mosip.registration.processor.packet.storage.utils.OSIUtils;
 import io.mosip.registration.processor.stages.operatorvalidator.OperatorValidationProcessor;
+import io.mosip.registration.processor.stages.operatorvalidator.OperatorValidator;
 
 @Configuration
 public class OperatorValidatorConfig {
@@ -12,6 +13,11 @@ public class OperatorValidatorConfig {
 	@Bean
 	public OperatorValidationProcessor getOperatorValidationProcessor() {
 		return new OperatorValidationProcessor();
+	}
+	
+	@Bean
+	public OperatorValidator getOperatorValidator() {
+		return new OperatorValidator();
 	}
 
 	@Bean
