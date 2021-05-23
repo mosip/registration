24b961fed81f3a78da2992a66dac4e34a549c681
Puneet Joshi <puneet@mosip.io>
2021-05-23 18:52:50 +0530

    [MOSIP-14346] Major overhaul.

--- db_scripts/mosip_regprc/ddl.sql
+++ db_scripts/mosip_regprc/ddl.sql
@@ -1,10 +1,9 @@
-\c mosip_regprc sysadmin
+\c mosip_regprc 
 
 \ir ddl/regprc-individual_demographic_dedup.sql
 \ir ddl/regprc-registration.sql
 \ir ddl/regprc-registration_list.sql
 \ir ddl/regprc-registration_transaction.sql
-
 \ir ddl/regprc-reg_manual_verification.sql
 \ir ddl/regprc-transaction_type.sql
 \ir ddl/regprc-rid_seq.sql
@@ -14,7 +13,5 @@
 \ir ddl/regprc-abis_request.sql
 \ir ddl/regprc-abis_response.sql
 \ir ddl/regprc-abis_response_det.sql
-
 \ir ddl/regprc-reg_lost_uin_det.sql
-
 \ir ddl/regprc-fk.sql
--- db_scripts/mosip_regprc/dml.sql
+++ db_scripts/mosip_regprc/dml.sql
@@ -1,14 +1,6 @@
-\c mosip_regprc sysadmin
-
-\set CSVDataPath '\'/home/dbadmin/mosip_regprc/'
-
--------------- Level 1 data load scripts ------------------------
-
+\c mosip_regprc 
 
 \COPY regprc.transaction_type (code,descr,lang_code,is_active,cr_by,cr_dtimes) FROM './dml/regprc-transaction_type.csv' delimiter ',' HEADER  csv;
-
-
----------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 
 
