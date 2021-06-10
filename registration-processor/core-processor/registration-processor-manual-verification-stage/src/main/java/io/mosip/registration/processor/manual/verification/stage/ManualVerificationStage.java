7be108955b153555469fa60aa44c39f60e31e880
Monobikash Das <43202165+MonobikashDas@users.noreply.github.com>
2021-06-10 13:07:47 +0530

    MOSIP-14844 : fix resend functionality (#1002)
    * MOSIP-14844 : initial commit
    
    * MOSIP-14844 : fix resend functionality
    
    * MOSIP-14844 : fix resend functionality
    
    * MOSIP-14844 : fix resend functionality
    
    * MOSIP-14844 : fix resend functionality
    
    * MOSIP-14844 : fix resend functionality
    
    Co-authored-by: Monobikash Das <M1045447@mindtree.com>

--- registration-processor/core-processor/registration-processor-manual-verification-stage/src/main/java/io/mosip/registration/processor/manual/verification/stage/ManualVerificationStage.java
+++ registration-processor/core-processor/registration-processor-manual-verification-stage/src/main/java/io/mosip/registration/processor/manual/verification/stage/ManualVerificationStage.java
@@ -157,8 +157,7 @@ public class ManualVerificationStage extends MosipVerticleAPIManager {
 	 */
 	public void deployStage() {
 		this.mosipEventBus = this.getEventBus(this, clusterManagerUrl, workerPoolSize);
-		this.consumeAndSend(mosipEventBus, MessageBusAddress.MANUAL_VERIFICATION_BUS_IN, 
-			MessageBusAddress.MANUAL_VERIFICATION_BUS_OUT, messageExpiryTimeLimit);
+		this.consume(mosipEventBus, MessageBusAddress.MANUAL_VERIFICATION_BUS_IN, messageExpiryTimeLimit);
 		queue = getQueueConnection();
 		if (queue != null) {
 
@@ -182,7 +181,7 @@ public class ManualVerificationStage extends MosipVerticleAPIManager {
 		router.setRoute(this.postUrl(mosipEventBus.getEventbus(), MessageBusAddress.MANUAL_VERIFICATION_BUS_IN, MessageBusAddress.MANUAL_VERIFICATION_BUS_OUT));
 		this.createServer(router.getRouter(), Integer.parseInt(port));
 	}
-	
+
 
 	public void sendMessage(MessageDTO messageDTO) {
 		this.send(this.mosipEventBus, MessageBusAddress.MANUAL_VERIFICATION_BUS_OUT, messageDTO);
