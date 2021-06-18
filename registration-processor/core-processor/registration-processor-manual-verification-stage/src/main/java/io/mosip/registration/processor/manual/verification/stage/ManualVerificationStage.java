3644a390891ba30ea451ec6fd0253ed275db895a
Monobikash Das <43202165+MonobikashDas@users.noreply.github.com>
2021-06-18 11:52:25 +0530

    MOR-96 : fixed hazelcast nullpointer (#1017)
    Co-authored-by: Monobikash Das <M1045447@mindtree.com>

--- registration-processor/core-processor/registration-processor-manual-verification-stage/src/main/java/io/mosip/registration/processor/manual/verification/stage/ManualVerificationStage.java
+++ registration-processor/core-processor/registration-processor-manual-verification-stage/src/main/java/io/mosip/registration/processor/manual/verification/stage/ManualVerificationStage.java
@@ -164,7 +164,7 @@ public class ManualVerificationStage extends MosipVerticleAPIManager {
 	 */
 	public void deployStage() {
 		this.mosipEventBus = this.getEventBus(this, clusterManagerUrl, workerPoolSize);
-		this.consumeAndSend(mosipEventBus, MessageBusAddress.MANUAL_VERIFICATION_BUS_IN, 
+		this.consumeAndSend(mosipEventBus, MessageBusAddress.MANUAL_VERIFICATION_BUS_IN,
 			MessageBusAddress.MANUAL_VERIFICATION_BUS_OUT, messageExpiryTimeLimit);
 		queue = getQueueConnection();
 		if (queue != null) {
