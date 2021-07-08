f6b1960b494e91ab7c0b69f8fbd11670c37f335f
M1022006 <M1022006@A2ML18485.mindtree.com>
2021-07-08 15:28:56 +0530

    MOSIP-11739 Code changes for packet status api

--- registration-processor/registration-processor-registration-status-service-impl/src/main/java/io/mosip/registration/processor/status/service/impl/PacketExternalStatusServiceImpl.java
+++ registration-processor/registration-processor-registration-status-service-impl/src/main/java/io/mosip/registration/processor/status/service/impl/PacketExternalStatusServiceImpl.java
@@ -1,17 +1,21 @@
 package io.mosip.registration.processor.status.service.impl;
 
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import io.mosip.kernel.core.logger.spi.Logger;
 import io.mosip.registration.processor.core.constant.LoggerFileConstant;
 import io.mosip.registration.processor.core.exception.util.PlatformErrorMessages;
 import io.mosip.registration.processor.core.logger.RegProcessorLogger;
-import io.mosip.registration.processor.status.code.RegistrationExternalStatusCode;
+import io.mosip.registration.processor.status.code.PacketExternalStatusCode;
+import io.mosip.registration.processor.status.code.RegistrationStatusCode;
 import io.mosip.registration.processor.status.dto.InternalRegistrationStatusDto;
 import io.mosip.registration.processor.status.dto.PacketExternalStatusDTO;
 import io.mosip.registration.processor.status.dto.RegistrationStatusDto;
@@ -21,7 +25,6 @@ import io.mosip.registration.processor.status.entity.SyncRegistrationEntity;
 import io.mosip.registration.processor.status.service.PacketExternalStatusService;
 import io.mosip.registration.processor.status.service.RegistrationStatusService;
 import io.mosip.registration.processor.status.service.SyncRegistrationService;
-import io.mosip.registration.processor.status.utilities.RegistrationExternalStatusUtility;
 
 @Component
 public class PacketExternalStatusServiceImpl implements PacketExternalStatusService {
@@ -29,16 +32,29 @@ public class PacketExternalStatusServiceImpl implements PacketExternalStatusServ
 	@Autowired
 	RegistrationStatusService<String, InternalRegistrationStatusDto, RegistrationStatusDto> registrationStatusService;
 
-	/** The regexternalstatus util. */
-	@Autowired
-	private RegistrationExternalStatusUtility regexternalstatusUtil;
-
 	/** The reg proc logger. */
 	private static Logger regProcLogger = RegProcessorLogger.getLogger(PacketExternalStatusServiceImpl.class);
 
 	/** The sync registration service. */
 	@Autowired
 	SyncRegistrationService<SyncResponseDto, SyncRegistrationDto> syncRegistrationService;
+
+	@Value("#{'${mosip.registration.processor.packet.status.transactiontypecodes-before-uploading-to-objectstore:PACKET_RECEIVER,SECUREZONE_NOTIFICATION}'.split(',')}")
+	private List<String> transactionTypeCodesBeforeUploadingToObjectStore;
+
+	@Value("#{'${mosip.registration.processor.packet.status.transactiontypecodes-uploading-to-objectstore:UPLOAD_PACKET}'.split(',')}")
+	private List<String> transactionTypeCodesUploadingToObjectStore;
+
+	@Value("#{'${mosip.registration.processor.packet.status.transactiontypecodes-time-based-resend-required:PACKET_RECEIVER}'.split(',')}")
+	private List<String> transactionTypeCodeTimeBasesResendRequired;
+
+	/** The elapsed time. */
+	@Value("${registration.processor.reprocess.elapse.time}")
+	private int elapsedTime;
+
+	/** The threshold time. */
+	@Value("${registration.processor.max.retry}")
+	private int maxRetryCount;
 
 	@Override
 	public List<PacketExternalStatusDTO> getByPacketIds(List<String> packetIdList) {
@@ -78,9 +94,10 @@ public class PacketExternalStatusServiceImpl implements PacketExternalStatusServ
 			InternalRegistrationStatusDto internalRegistrationStatusDto) {
 		PacketExternalStatusDTO packetStatusDTO = new PacketExternalStatusDTO();
 		if (internalRegistrationStatusDto.getStatusCode() != null) {
-			RegistrationExternalStatusCode registrationExternalStatusCode = null;// regexternalstatusUtil.getExternalStatus(entity);
+			PacketExternalStatusCode packetExternalStatusCode = getPacketExternalStatusCode(
+					internalRegistrationStatusDto);
 
-			String mappedValue = registrationExternalStatusCode.toString();
+			String mappedValue = packetExternalStatusCode.toString();
 
 			packetStatusDTO.setStatusCode(mappedValue);
 		} else {
@@ -89,6 +106,52 @@ public class PacketExternalStatusServiceImpl implements PacketExternalStatusServ
 					PlatformErrorMessages.RPR_RGS_REGISTRATION_STATUS_NOT_EXIST.getMessage());
 		}
 		return packetStatusDTO;
+	}
+
+	private PacketExternalStatusCode getPacketExternalStatusCode(
+			InternalRegistrationStatusDto internalRegistrationStatusDto) {
+		PacketExternalStatusCode mappedValue = null;
+		String status = internalRegistrationStatusDto.getStatusCode();
+		if (transactionTypeCodesBeforeUploadingToObjectStore
+				.contains(internalRegistrationStatusDto.getLatestTransactionTypeCode())
+				|| transactionTypeCodesUploadingToObjectStore
+						.contains(internalRegistrationStatusDto.getLatestTransactionTypeCode())) {
+			if (status.equalsIgnoreCase(RegistrationStatusCode.PROCESSING.toString())
+					|| status.equalsIgnoreCase(RegistrationStatusCode.REPROCESS.toString())
+					|| status.equalsIgnoreCase(RegistrationStatusCode.RESUMABLE.toString())) {
+				if (transactionTypeCodeTimeBasesResendRequired
+						.contains(internalRegistrationStatusDto.getLatestTransactionTypeCode())) {
+					long timeElapsedinPacketreceiver = checkElapsedTime(internalRegistrationStatusDto);
+					if (timeElapsedinPacketreceiver > elapsedTime) {
+						if ((internalRegistrationStatusDto.getRetryCount() < maxRetryCount)) {
+							mappedValue = PacketExternalStatusCode.RESEND;
+						} else {
+							mappedValue = PacketExternalStatusCode.PROCESSED;// REJECTED?
+						}
+					} else {
+						mappedValue = PacketExternalStatusCode.PROCESSING;
+					}
+				}else {
+					mappedValue = PacketExternalStatusCode.PROCESSING;
+				}
+           
+			} else if (status.equalsIgnoreCase(RegistrationStatusCode.FAILED.toString())) {
+				if ((internalRegistrationStatusDto.getRetryCount() < maxRetryCount)) {
+					mappedValue = PacketExternalStatusCode.RESEND;
+				} else {
+					mappedValue = PacketExternalStatusCode.PROCESSED;
+				}
+			}
+			else if (!transactionTypeCodesUploadingToObjectStore
+					.contains(internalRegistrationStatusDto.getLatestTransactionTypeCode())) {
+				mappedValue = PacketExternalStatusCode.PROCESSING;
+			} else {
+				mappedValue = PacketExternalStatusCode.PROCESSED;
+			}
+		} else {
+			mappedValue = PacketExternalStatusCode.PROCESSED;
+		}
+		return mappedValue;
 	}
 
 	private List<PacketExternalStatusDTO> convertEntityListToDtoListAndGetExternalStatusForPacket(
@@ -105,7 +168,15 @@ public class PacketExternalStatusServiceImpl implements PacketExternalStatusServ
 	private PacketExternalStatusDTO convertEntityToDtoAndGetExternalStatusForPacket(SyncRegistrationEntity entity) {
 		PacketExternalStatusDTO packetStatusDTO = new PacketExternalStatusDTO();
 		packetStatusDTO.setPacketId(entity.getPacketId());
-		packetStatusDTO.setStatusCode(RegistrationExternalStatusCode.UPLOAD_PENDING.toString());
+		packetStatusDTO.setStatusCode(PacketExternalStatusCode.UPLOAD_PENDING.toString());
 		return packetStatusDTO;
+	}
+
+	private Long checkElapsedTime(InternalRegistrationStatusDto internalRegistrationStatusDto) {
+		LocalDateTime createdTime = internalRegistrationStatusDto.getLatestTransactionTimes();
+		LocalDateTime currentTime = LocalDateTime.now();
+		LocalDateTime tempDate = LocalDateTime.from(createdTime);
+		long seconds = tempDate.until(currentTime, ChronoUnit.SECONDS);
+		return seconds;
 	}
 }
