fedb2991af8d8d4539be8bb2d1b89b12f2b80be2
M1030448 <M1030448@MT-2G2B2B3>
2021-10-14 16:10:00 +0530

    MOSIP-18087

--- registration-processor/registration-processor-registration-status-service-impl/src/main/java/io/mosip/registration/processor/status/service/impl/SyncRegistrationServiceImpl.java
+++ registration-processor/registration-processor-registration-status-service-impl/src/main/java/io/mosip/registration/processor/status/service/impl/SyncRegistrationServiceImpl.java
@@ -526,6 +526,7 @@ public class SyncRegistrationServiceImpl implements SyncRegistrationService<Sync
 			syncRegistration = convertDtoToEntity(registrationDto, referenceId, timeStamp);
 			syncRegistration.setWorkflowInstanceId(existingSyncRegistration.getWorkflowInstanceId());
 			syncRegistration.setCreateDateTime(existingSyncRegistration.getCreateDateTime());
+			syncRegistration.setRegistrationDate(syncRegistration.getCreateDateTime().toLocalDate());
 			syncRegistrationDao.update(syncRegistration);
 			syncResponseDto.setRegistrationId(registrationDto.getRegistrationId());
 
@@ -536,6 +537,7 @@ public class SyncRegistrationServiceImpl implements SyncRegistrationService<Sync
 			syncRegistration = convertDtoToEntity(registrationDto, referenceId, timeStamp);
 			syncRegistration.setCreateDateTime(LocalDateTime.now(ZoneId.of("UTC")));
 			syncRegistration.setWorkflowInstanceId(RegistrationUtility.generateId());
+			syncRegistration.setRegistrationDate(syncRegistration.getCreateDateTime().toLocalDate());
 			syncRegistrationDao.save(syncRegistration);
 			syncResponseDto.setRegistrationId(registrationDto.getRegistrationId());
 			
@@ -669,10 +671,6 @@ public class SyncRegistrationServiceImpl implements SyncRegistrationService<Sync
 			syncRegistrationEntity.setPhone(dto.getPhone() != null ? getHashCode(dto.getPhone()) : null);
 			syncRegistrationEntity
 					.setPostalCode(getHashCode(getPostalCode(referenceId.split("_")[0], dto.getLangCode())));
-			if (dto.getCreateDateTime() != null) {
-				syncRegistrationEntity
-						.setRegistrationDate(dto.getCreateDateTime().toLocalDate());
-			}
 		} catch (JsonProcessingException | RegStatusAppException | EncryptionFailureException
 				| ApisResourceAccessException exception) {
 			regProcLogger.error(LoggerFileConstant.SESSIONID.toString(), LoggerFileConstant.REGISTRATIONID.toString(),
