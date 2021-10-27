4bd9455c6304d0e9a783e2c03e849268d66f373e
kameshsr <47484458+kameshsr@users.noreply.github.com>
2021-10-27 11:00:20 +0530

    Mosip 15857 Fixed https to http redirect issue for registration status service. (#1227)
    * MOSIP-15857: Fixed For Swagger Implementation
    
    * MOSIP:15857: Fixed https to http redirect issue for registration status service
    
    * MOSIP:15857 Fixed naming case
    
    * Removed hard coded value of localhost
    
    * Added openapi properties
    
    * Updated openapi properties
    
    * Added openapi properties
    
    Signed-off-by: Sowmya Ujjappa Banakar <sowmya.61022006@ltimindtree.com>

--- registration-processor/init/registration-processor-registration-status-service/src/main/java/io/mosip/registration/processor/status/api/config/RegistrationStatusConfig.java
+++ registration-processor/init/registration-processor-registration-status-service/src/main/java/io/mosip/registration/processor/status/api/config/RegistrationStatusConfig.java
@@ -1,26 +1,62 @@
 package io.mosip.registration.processor.status.api.config;
 
-import io.swagger.v3.oas.models.Components;
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-/**
- * The Class RegistrationStatusConfig.
- */
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.servers.Server;
 
+/**
+ * Configuration class for swagger config
+ * @implSpec upgrade the Swagger2.0 to OpenAPI (Swagger3.0)
+ *
+ */
 @Configuration
 public class RegistrationStatusConfig {
 
-	/**
-	 * Registration status bean.
-	 */
+	private static final Logger logger = LoggerFactory.getLogger(RegistrationStatusConfig.class);
+
+	@Autowired
+	private OpenApiProperties openApiProperties;
+
 	@Bean
-	public OpenAPI customOpenAPI() {
-		return new OpenAPI()
-				.components(new Components())
-				.info(new Info().title("Registration Status Service API documentation").description(
-						"Registration status service contains the APIs used by registration client and resident services to sync packets and check the status the packets").version("3.0.1"));
-	}
+    public OpenAPI openApi() {
+		String msg = "Swagger open api, ";
+		OpenAPI api = new OpenAPI()
+                .components(new Components());
+		if (null != openApiProperties.getInfo()) {
+			api.info(new Info()
+				.title(openApiProperties.getInfo().getTitle())
+				.version(openApiProperties.getInfo().getVersion())
+				.description(openApiProperties.getInfo().getDescription()));
+			if (null != openApiProperties.getInfo().getLicense()) {
+				api.getInfo().license(new License()
+						.name(openApiProperties.getInfo().getLicense().getName())
+						.url(openApiProperties.getInfo().getLicense().getUrl()));
+				logger.info(msg + "info license property is added");
+			} else {
+				logger.error(msg + "info license property is empty");
+			}
+			logger.info(msg + "info property is added");
+		} else {
+			logger.error(msg + "info property is empty");
+		}
+
+		if (null != openApiProperties.getRegistrationProcessorStatusService().getServers()) {
+			openApiProperties.getRegistrationProcessorStatusService().getServers().forEach(server -> {
+				api.addServersItem(new Server().description(server.getDescription()).url(server.getUrl()));
+			});
+			logger.info(msg + "server property is added");
+		} else {
+			logger.error(msg + "server property is empty");
+		}
+		return api;
+    }
+
 }
