872127aa45f4cfa961eae924398038ba98f0db48
M1030448 <M1030448@MT-2G2B2B3>
2021-11-02 10:38:47 +0530

    MOSIP-13522

--- registration-processor/registration-processor-registration-status-service-impl/src/main/java/io/mosip/registration/processor/status/repositary/RegistrationRepositary.java
+++ registration-processor/registration-processor-registration-status-service-impl/src/main/java/io/mosip/registration/processor/status/repositary/RegistrationRepositary.java
@@ -33,6 +33,6 @@ public interface RegistrationRepositary<T extends BaseRegistrationEntity, E> ext
 	public List<String> getWithoutStatusCodes(@Param("regIds") List<String> regIds,
 													   @Param("statusCode1") String statusCode1, @Param("statusCode2") String statusCode2);
 	
-	@Query("SELECT COUNT(workflowInstanceId)  FROM RegistrationStatusEntity registration WHERE registration.statusCode=:statusCode AND registration.latestTransactionStatusCode =:latestTransactionStatusCode AND registration.latestTransactionTimes < :timeDifference")
+	@Query("SELECT COUNT(registration.workflowInstanceId)  FROM RegistrationStatusEntity registration WHERE registration.statusCode=:statusCode AND registration.latestTransactionStatusCode =:latestTransactionStatusCode AND registration.latestTransactionTimes < :timeDifference")
 	long countByStatusCode(@Param("statusCode")String statusCode,@Param("latestTransactionStatusCode")String latestTransactionStatusCode,@Param("timeDifference")LocalDateTime timeDifference);
 }
