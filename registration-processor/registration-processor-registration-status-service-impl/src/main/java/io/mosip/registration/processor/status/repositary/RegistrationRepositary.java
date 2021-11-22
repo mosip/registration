10257957b49b7d142294a38f7345e5120bd553c4
M1030448 <M1030448@MT-2G2B2B3>
2021-11-22 18:16:46 +0530

    MOSIP-18418_MOSIP-18421

--- registration-processor/registration-processor-registration-status-service-impl/src/main/java/io/mosip/registration/processor/status/repositary/RegistrationRepositary.java
+++ registration-processor/registration-processor-registration-status-service-impl/src/main/java/io/mosip/registration/processor/status/repositary/RegistrationRepositary.java
@@ -12,6 +12,7 @@ import org.springframework.stereotype.Repository;
 
 import io.mosip.kernel.core.dataaccess.spi.repository.BaseRepository;
 import io.mosip.registration.processor.status.entity.BaseRegistrationEntity;
+import io.mosip.registration.processor.status.entity.RegistrationStatusEntity;
 import io.mosip.registration.processor.status.entity.SyncRegistrationEntity;
 
 /**
@@ -36,5 +37,37 @@ public interface RegistrationRepositary<T extends BaseRegistrationEntity, E> ext
 	@Query("SELECT COUNT(registration.id.workflowInstanceId)  FROM RegistrationStatusEntity registration WHERE registration.statusCode=:statusCode AND registration.latestTransactionStatusCode =:latestTransactionStatusCode AND registration.latestTransactionTimes < :timeDifference")
 	long getInReprocessPacketsCount(@Param("statusCode")String statusCode,@Param("latestTransactionStatusCode")String latestTransactionStatusCode,@Param("timeDifference")LocalDateTime timeDifference);
 
+	@Query("SELECT registration FROM RegistrationStatusEntity registration WHERE registration.regId = :regId and registration.isDeleted =false and registration.isActive=true")
+	public List<RegistrationStatusEntity> findByRegId(@Param("regId") String regId);
 	
+	@Query("SELECT registration FROM RegistrationStatusEntity registration WHERE registration.regId IN :regIds and registration.isDeleted =false and registration.isActive=true")
+	public List<RegistrationStatusEntity> findByRegIds(@Param("regIds") List<String> regIds);
+	
+	@Query("SELECT registration FROM RegistrationStatusEntity registration WHERE registration.regId IN :regIds and registration.isDeleted =false and registration.isActive=true order by registration.createDateTime")
+	public List<RegistrationStatusEntity> findByRegIdsOrderbyCreatedDateTime(@Param("regIds") List<String> regIds);
+	
+	@Query("SELECT registration FROM RegistrationStatusEntity registration WHERE registration.statusCode = :statusCode order by registration.updateDateTime limit :fetchSize")
+	public List<RegistrationStatusEntity> findByStatusCodeOrderbyUpdatedDateTime(@Param("statusCode") String statusCode,@Param("fetchSize") Integer fetchSize);
+	
+	@Query("SELECT registration FROM RegistrationStatusEntity registration WHERE registration.statusCode = :statusCode and registration.isDeleted =false and registration.isActive=true")
+	public List<RegistrationStatusEntity> findByStatusCode(@Param("statusCode") String statusCode);
+	
+	@Query("SELECT registration FROM RegistrationStatusEntity registration WHERE registration.regId = :regId and registration.statusCode = :statusCode ")
+	public List<RegistrationStatusEntity> findByRegIdANDByStatusCode(@Param("regId") String regId,@Param("statusCode") String statusCode);
+	
+	@Query("SELECT registration FROM RegistrationStatusEntity registration WHERE registration.id.workflowInstanceId = :workflowInstanceId and registration.isDeleted =false and registration.isActive=true")
+	public List<RegistrationStatusEntity> findByWorkflowInstanceId(@Param("workflowInstanceId") String workflowInstanceId);
+	
+	@Query("SELECT registration FROM RegistrationStatusEntity registration WHERE registration.latestTransactionStatusCode IN :status and registration.regProcessRetryCount<=:reprocessCount and registration.latestTransactionTimes<:timeDifference and registration.statusCode  NOT IN :statusCodes limit :fetchSize")
+	public List<RegistrationStatusEntity> getUnProcessedPackets(@Param("status") List<String> status,@Param("reprocessCount") Integer reprocessCount,@Param("timeDifference") LocalDateTime timeDifference,@Param("statusCodes") List<String> statusCodes,@Param("fetchSize") Integer fetchSize );
+	
+	@Query("SELECT COUNT(*) FROM RegistrationStatusEntity registration WHERE registration.latestTransactionStatusCode IN :status and registration.regProcessRetryCount<=:reprocessCount and registration.latestTransactionTimes<:timeDifference and registration.statusCode  NOT IN :statusCodes ")
+	public int getUnProcessedPacketsCount(@Param("status") List<String> status,@Param("reprocessCount") Integer reprocessCount,@Param("timeDifference") LocalDateTime timeDifference,@Param("statusCodes") List<String> statusCodes );
+	
+	@Query("SELECT registration FROM RegistrationStatusEntity registration WHERE registration.statusCodes IN :statusCodes  and registration.resumeTimeStamp<:now() and registration.defaultResumeAction is not null order by registration.updateDateTime limit :fetchSize")
+	public List<RegistrationStatusEntity> getActionablePausedPackets(@Param("statusCodes") List<String> statusCodes,@Param("fetchSize") Integer fetchSize);
+
+	@Query("SELECT registration FROM RegistrationStatusEntity registration WHERE registration.statusCode=:statusCode   order by registration.updateDateTime limit :fetchSize")
+	public List<RegistrationStatusEntity> getResumablePackets(@Param("statusCode") String statusCode,@Param("fetchSize") Integer fetchSize);
 }
+
--- registration-processor/registration-processor-registration-status-service-impl/src/main/java/io/mosip/registration/processor/status/repositary/SyncRegistrationRepository.java
+++ registration-processor/registration-processor-registration-status-service-impl/src/main/java/io/mosip/registration/processor/status/repositary/SyncRegistrationRepository.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Repository;
 
 import io.mosip.kernel.core.dataaccess.spi.repository.BaseRepository;
 import io.mosip.registration.processor.status.entity.BaseSyncRegistrationEntity;
+import io.mosip.registration.processor.status.entity.RegistrationStatusEntity;
 import io.mosip.registration.processor.status.entity.SyncRegistrationEntity;
 
 @Repository
@@ -16,5 +17,30 @@ public interface SyncRegistrationRepository<T extends BaseSyncRegistrationEntity
 	@Query("SELECT registrationList FROM SyncRegistrationEntity registrationList WHERE registrationList.registrationId =:regId and registrationList.registrationType =:regType")
 	public List<SyncRegistrationEntity> getSyncRecordsByRegIdAndRegType(@Param("regId") String regId,
 			@Param("regType") String regType);
+	
+	@Query("SELECT registrationList FROM SyncRegistrationEntity registrationList WHERE registrationList.registrationId = :registrationId and registrationList.isDeleted =false ")
+	public List<SyncRegistrationEntity> findByRegistrationId(@Param("registrationId") String registrationId);
+	
+	@Query("SELECT registrationList FROM SyncRegistrationEntity registrationList WHERE registrationList.packetId = :packetId and registrationList.isDeleted =false ")
+	public List<SyncRegistrationEntity> findByPacketId(@Param("packetId") String packetId);
+	
+	@Query("SELECT registrationList FROM SyncRegistrationEntity registrationList WHERE registrationList.additionalInfoReqId = :additionalInfoReqId and registrationList.isDeleted =false ")
+	public List<SyncRegistrationEntity> findByAdditionalInfoReqId(@Param("additionalInfoReqId") String additionalInfoReqId);
+	
+	@Query("SELECT registrationList FROM SyncRegistrationEntity registrationList WHERE registrationList.registrationId IN :registrationIds and registrationList.isDeleted =false ")
+	public List<SyncRegistrationEntity> findByRegistrationIds(@Param("registrationIds") List<String> registrationIds);
+	
+	@Query("SELECT registrationList FROM SyncRegistrationEntity registrationList WHERE registrationList.packetId IN :packetIds and registrationList.isDeleted =false ")
+	public List<SyncRegistrationEntity> findByPacketIds(@Param("packetIds") List<String> packetIds);
+	
+	@Query("SELECT registrationList FROM SyncRegistrationEntity registrationList WHERE registrationList.registrationId = :registrationId and registrationList.additionalInfoReqId = :additionalInfoReqId and registrationList.isDeleted =false ")
+	public List<SyncRegistrationEntity> findByRegistrationIdIdAndAdditionalInfoReqId(@Param("registrationId") String registrationId,@Param("additionalInfoReqId") String additionalInfoReqId);
+	
+	@Query("SELECT registrationList FROM SyncRegistrationEntity registrationList WHERE registrationList.registrationId = :registrationId and registrationList.registrationType = :registrationType and registrationList.isDeleted =false ")
+	public List<SyncRegistrationEntity> findByRegistrationIdIdAndRegType(@Param("registrationId") String registrationId,@Param("registrationType") String registrationType);
+	
+	
+	@Query("SELECT registrationList FROM SyncRegistrationEntity registrationList WHERE registrationList.workflowInstanceId = :workflowInstanceId and registrationList.isDeleted =false ")
+	public List<SyncRegistrationEntity> findByworkflowInstanceId(@Param("workflowInstanceId") String workflowInstanceId);
 
 }
