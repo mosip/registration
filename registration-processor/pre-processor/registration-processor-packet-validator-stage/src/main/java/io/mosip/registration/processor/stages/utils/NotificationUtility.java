db2e5c43f941b885a0945399a21f5b609bc3fdec
M1030448 <M1030448@MT-2G2B2B3>
2021-12-20 16:27:39 +0530

    MOSIP-11009

--- registration-processor/pre-processor/registration-processor-packet-validator-stage/src/main/java/io/mosip/registration/processor/stages/utils/NotificationUtility.java
+++ registration-processor/pre-processor/registration-processor-packet-validator-stage/src/main/java/io/mosip/registration/processor/stages/utils/NotificationUtility.java
@@ -17,6 +17,7 @@ import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.json.JSONArray;
 import org.json.JSONException;
+import org.json.simple.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
@@ -54,6 +55,7 @@ import io.mosip.registration.processor.message.sender.exception.TemplateGenerati
 import io.mosip.registration.processor.message.sender.exception.TemplateNotFoundException;
 import io.mosip.registration.processor.message.sender.template.TemplateGenerator;
 import io.mosip.registration.processor.packet.storage.utils.PriorityBasedPacketManagerService;
+import io.mosip.registration.processor.packet.storage.utils.Utilities;
 import io.mosip.registration.processor.rest.client.utils.RestApiClient;
 import io.mosip.registration.processor.stages.dto.MessageSenderDTO;
 import io.mosip.registration.processor.status.dto.InternalRegistrationStatusDto;
@@ -110,6 +112,10 @@ public class NotificationUtility {
 	
 	@Autowired
 	private PriorityBasedPacketManagerService packetManagerService;
+	
+	/** The utility. */
+	@Autowired
+	private Utilities utility;
 
 	private static final String SMS_SERVICE_ID = "mosip.registration.processor.sms.id";
 	private static final String REG_PROC_APPLICATION_VERSION = "mosip.registration.processor.application.version";
@@ -143,11 +149,15 @@ public class NotificationUtility {
 		Map<String, Object> attributes = new HashMap<>();
 		attributes.put("RID", registrationId);
 		List<String> preferredLanguages=getPreferredLanguages(registrationStatusDto);
+		JSONObject regProcessorIdentityJson = utility.getRegistrationProcessorMappingJson(MappingJsonConstants.IDENTITY);
+        String nameField = JsonUtil.getJSONValue(
+                JsonUtil.getJSONObject(regProcessorIdentityJson, MappingJsonConstants.NAME),
+                MappingJsonConstants.VALUE);
 		for(String preferredLanguage:preferredLanguages) {
 		if (registrationAdditionalInfoDTO.getName() != null) {
-			attributes.put("name_"+preferredLanguage , registrationAdditionalInfoDTO.getName());
+			attributes.put(nameField , registrationAdditionalInfoDTO.getName());
 		} else {
-			attributes.put("name_"+ preferredLanguage, "");
+			attributes.put(nameField, "");
 		}
 		
 		if (isProcessingSuccess) {
