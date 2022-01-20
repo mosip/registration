0c2f14c2e3e477f915c4f94ef1467128aa7f405e
M1030448 <M1030448@MT-2G2B2B3>
2022-01-20 14:49:26 +0530

    reverting changes

--- registration-processor/workflow-engine/registration-processor-workflow-manager-service/src/test/java/io/mosip/registration/processor/workflowmanager/util/test/WebSubUtilTest.java
+++ registration-processor/workflow-engine/registration-processor-workflow-manager-service/src/test/java/io/mosip/registration/processor/workflowmanager/util/test/WebSubUtilTest.java
@@ -3,16 +3,11 @@ package io.mosip.registration.processor.workflowmanager.util.test;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
 
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.env.Environment;
 import org.springframework.http.HttpHeaders;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.util.ReflectionTestUtils;
@@ -34,15 +29,6 @@ public class WebSubUtilTest {
 
 	@InjectMocks
 	WebSubUtil webSubUtil;
-	
-
-	@Mock
-	Environment environment;
-
-	@Before
-	public void setup() {
-		when(environment.getProperty(Mockito.anyString())).thenReturn("").thenReturn("").thenReturn("").thenReturn("").thenReturn("").thenReturn("");
-	}
 
 	@Test
 	public void testPublishEventSuccess() throws WebSubClientException {
