691dbcb4b8d66ce4fca0715748881afdb4b5e2a2
M1030448 <M1030448@MT-2G2B2B3>
2022-02-01 10:08:24 +0530

    # WARNING: head commit changed in the meantime
    Merge branch '1.2.0-rc2' of
    https://github.com/JyotiPrakashNayak/registration.git into 1.2.0-rc2

--- registration-processor/workflow-engine/registration-processor-workflow-manager-service/src/main/java/io/mosip/registration/processor/workflowmanager/WorkflowManagerApplication.java
+++ registration-processor/workflow-engine/registration-processor-workflow-manager-service/src/main/java/io/mosip/registration/processor/workflowmanager/WorkflowManagerApplication.java
@@ -12,6 +12,7 @@ public class WorkflowManagerApplication
 	public static void main(String[] args) {
 		AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext();
 		ctx.scan("io.mosip.registration.processor.core.config",
+				"io.mosip.kernel.auth.defaultadapter.*",
 				"io.mosip.registration.processor.workflowmanager.config",
 				"io.mosip.registration.processor.status.config",
 				"io.mosip.registration.processor.core.kernel.beans",
