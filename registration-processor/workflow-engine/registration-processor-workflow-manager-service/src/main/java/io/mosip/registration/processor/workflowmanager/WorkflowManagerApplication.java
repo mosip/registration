8e126db55780a113dec0a9db573c270cef65df5a
JyotiPrakashNayak <jyoti.nayak@mindtree.com>
2022-02-03 18:35:12 +0530

    Notification changes (#1429)
    * added build layer
    
    * modified cloud config properties
    
    * revert
    
    * Notification changes
    
    Co-authored-by: M1030448 <M1030448@MT-2G2B2B3>

--- registration-processor/workflow-engine/registration-processor-workflow-manager-service/src/main/java/io/mosip/registration/processor/workflowmanager/WorkflowManagerApplication.java
+++ registration-processor/workflow-engine/registration-processor-workflow-manager-service/src/main/java/io/mosip/registration/processor/workflowmanager/WorkflowManagerApplication.java
@@ -1,24 +1,33 @@
 package io.mosip.registration.processor.workflowmanager;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
 import io.mosip.registration.processor.workflowmanager.verticle.WorkflowActionApi;
 import io.mosip.registration.processor.workflowmanager.verticle.WorkflowActionJob;
 import io.mosip.registration.processor.workflowmanager.verticle.WorkflowInternalActionVerticle;
 
-
+@Configuration
+@EnableAutoConfiguration
+@SpringBootApplication
+@ComponentScan(basePackages = { "io.mosip.registration.processor.core.config",
+		"${mosip.auth.adapter.impl.basepackage}",
+		"io.mosip.registration.processor.workflowmanager.config",
+		"io.mosip.registration.processor.status.config",
+		"io.mosip.registration.processor.core.kernel.beans",
+		"io.mosip.registration.processor.packet.storage.config",
+		"io.mosip.kernel.websub.api.config.publisher" })
 public class WorkflowManagerApplication 
 {
 	public static void main(String[] args) {
-		AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext();
-		ctx.scan("io.mosip.registration.processor.core.config",
-				"${mosip.auth.adapter.impl.basepackage}",
-				"io.mosip.registration.processor.workflowmanager.config",
-				"io.mosip.registration.processor.status.config",
-				"io.mosip.registration.processor.core.kernel.beans",
-				"io.mosip.registration.processor.packet.storage.config",
-				"io.mosip.kernel.websub.api.config.publisher");
-		ctx.refresh();
+		ConfigurableApplicationContext ctx = SpringApplication.run(WorkflowManagerApplication.class, args);
+		
+		
 		WorkflowInternalActionVerticle workflowInternalActionVerticle = ctx
 				.getBean(WorkflowInternalActionVerticle.class);
 		workflowInternalActionVerticle.deployVerticle();
