d745247f88baa8d9c585038805590afe3ef22dcd
Monobikash Das <43202165+MonobikashDas@users.noreply.github.com>
2022-03-07 17:06:08 +0530

    MOSIP-19244 : using selfTokenRestTemplate (#1452)
    * MOSIP-19244 : using selfTokenRestTemplate
    
    * MOSIP-20472 : added vid in template

--- registration-processor/workflow-engine/registration-processor-reprocessor/src/main/java/io/mosip/registration/processor/reprocessor/config/ReprocessorConfigBeans.java
+++ registration-processor/workflow-engine/registration-processor-reprocessor/src/main/java/io/mosip/registration/processor/reprocessor/config/ReprocessorConfigBeans.java
@@ -3,7 +3,6 @@ package io.mosip.registration.processor.reprocessor.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.web.client.RestTemplate;
 
 import io.mosip.registration.processor.core.spi.restclient.RegistrationProcessorRestClientService;
 import io.mosip.registration.processor.packet.storage.utils.PacketManagerService;
@@ -30,14 +29,8 @@ public class ReprocessorConfigBeans {
 		return new RegistrationProcessorRestClientServiceImpl();
 	}
 
-
 	@Bean
 	public PacketManagerService getPacketManagerService() {
 		return new PacketManagerService();
-	}
-
-	@Bean
-	public RestTemplate getRestTemplate() {
-		return new RestTemplate();
 	}
 }
