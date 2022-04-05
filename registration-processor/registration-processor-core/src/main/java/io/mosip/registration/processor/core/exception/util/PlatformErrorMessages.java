835dc4e0efb3bbf2e6ffc4e292aca1b742a2e116
Shilpa-Manjunath <72008366+Shilpa-Manjunath@users.noreply.github.com>
2022-04-05 13:27:18 +0530

    added exception (#1495)
    Co-authored-by: Shilpa M <m1053288@mindtree.com>

--- registration-processor/registration-processor-core/src/main/java/io/mosip/registration/processor/core/exception/util/PlatformErrorMessages.java
+++ registration-processor/registration-processor-core/src/main/java/io/mosip/registration/processor/core/exception/util/PlatformErrorMessages.java
@@ -201,10 +201,10 @@ public enum PlatformErrorMessages {
 	
 	/** The rpr rgs io exception */
 	RPR_RGS_IOEXCEPTION(PlatformConstants.RPR_REGISTRATION_STATUS_MODULE + "035" , "IO Exception"),
-	
+
 	LINK_FOR_USERID_INDIVIDUALID_FAILED_STATUS_EXCEPTION(PlatformConstants.RPR_REGISTRATION_STATUS_MODULE + "035",
 			"Unable to get the IndividualId for UserId"),
-	
+
 	UNABLE_TO_ACCESS_API(PlatformConstants.RPR_REGISTRATION_STATUS_MODULE + "036",
 			"Unable to access API"),
 
@@ -1034,10 +1034,12 @@ public enum PlatformErrorMessages {
 	/** The stage processing failed. */
 	RPR_SYS_STAGE_PROCESSING_FAILED(PlatformConstants.RPR_SYSTEM_EXCEPTION + "021",
 			"Stage processing of message failed with an exception"),
-	
+
 	/** The rpr sys connection exception. */
 	RPR_SYS_QUEUE_CONNECTION_EXCEPTION(PlatformConstants.RPR_SYSTEM_EXCEPTION + "022",
 			"Error while connecting to active mq"),
+
+
 
 	// Cbeff Util Exceptions
 	/** The rpr utl biometric tag match. */
@@ -1438,12 +1440,12 @@ public enum PlatformErrorMessages {
 			"Draft request is unavaialble in id-repo draft repository."),
 	RPR_FINALIZATION_STAGE_API_RESOURCE_EXCEPTION(PlatformConstants.RPR_FINALIZATION_STAGE + "004",
 			"Not able to access the API resource"),
-	
+
 	INDIVIDUAL_BIOMETRIC_AUTHENTICATION_FAILED(PlatformConstants.RPR_BIOMETRIC_AUTHENTICATION_MODULE + "004",
 			"Bio authentication failed")
 	;
-	
-	
+
+
 
 	/** The error message. */
 	private final String errorMessage; 
