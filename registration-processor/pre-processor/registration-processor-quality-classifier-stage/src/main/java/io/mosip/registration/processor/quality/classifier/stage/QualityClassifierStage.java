cbc95a04d989ab36d22fc5ddb98ff37ef088aa2e
jyoti <jyoti.nayak@mindtree.com>
2022-06-07 19:06:44 +0530

    quality

--- registration-processor/pre-processor/registration-processor-quality-classifier-stage/src/main/java/io/mosip/registration/processor/quality/classifier/stage/QualityClassifierStage.java
+++ registration-processor/pre-processor/registration-processor-quality-classifier-stage/src/main/java/io/mosip/registration/processor/quality/classifier/stage/QualityClassifierStage.java
@@ -442,6 +442,9 @@ public class QualityClassifierStage extends MosipVerticleAPIManager {
 			BiometricType biometricType = bir.getBdbInfo().getType().get(0);
 			BIR[] birArray = new BIR[1];
 			birArray[0] = bir;
+			if(biometricType.name().equalsIgnoreCase(BiometricType.IRIS.name()) ||
+					biometricType.name().equalsIgnoreCase(BiometricType.FACE.name()) ||
+					biometricType.name().equalsIgnoreCase(BiometricType.FINGER.name())) {
 			float[] qualityScoreresponse = getBioSdkInstance(biometricType).getSegmentQuality(birArray, null);
 
 			float score = qualityScoreresponse[0];
@@ -452,7 +455,7 @@ public class QualityClassifierStage extends MosipVerticleAPIManager {
 
 			bioTypeMinScoreMap.put(bioType,
 					storedMinScore == null ? score : storedMinScore > score ? score : storedMinScore);
-
+			}
 		}
 
 		for (Entry<String, Float> bioTypeMinEntry : bioTypeMinScoreMap.entrySet()) {
