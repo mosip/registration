fa730a4ccbeed1d68dcad287fef34ccaafd38639
jyoti <jyoti.nayak@mindtree.com>
2022-06-08 17:56:37 +0530

    MOSIP-22116

--- registration-processor/pre-processor/registration-processor-quality-classifier-stage/src/main/java/io/mosip/registration/processor/quality/classifier/stage/QualityClassifierStage.java
+++ registration-processor/pre-processor/registration-processor-quality-classifier-stage/src/main/java/io/mosip/registration/processor/quality/classifier/stage/QualityClassifierStage.java
@@ -442,9 +442,7 @@ public class QualityClassifierStage extends MosipVerticleAPIManager {
 			BiometricType biometricType = bir.getBdbInfo().getType().get(0);
 			BIR[] birArray = new BIR[1];
 			birArray[0] = bir;
-			if(biometricType.name().equalsIgnoreCase(BiometricType.IRIS.name()) ||
-					biometricType.name().equalsIgnoreCase(BiometricType.FACE.name()) ||
-					biometricType.name().equalsIgnoreCase(BiometricType.FINGER.name())) {
+			if(!biometricType.name().equalsIgnoreCase(BiometricType.EXCEPTION_PHOTO.name()) ) {
 			float[] qualityScoreresponse = getBioSdkInstance(biometricType).getSegmentQuality(birArray, null);
 
 			float score = qualityScoreresponse[0];
