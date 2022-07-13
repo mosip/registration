1fdff3e6fdf010265bc81ea00a9384b121e88cd0
NishantKumar4559 <100404536+NishantKumar4559@users.noreply.github.com>
2022-07-13 11:49:14 +0530

    Update ddl.sql (#1567)
    added verification db script
    
    Signed-off-by: Dhanendra Sahu <dhanendra.tech@gmail.com>

--- db_scripts/mosip_regprc/ddl.sql
+++ db_scripts/mosip_regprc/ddl.sql
@@ -5,6 +5,7 @@
 \ir ddl/regprc-registration_list.sql
 \ir ddl/regprc-registration_transaction.sql
 \ir ddl/regprc-reg_manual_verification.sql
+\ir ddl/regprc-reg_verification.sql
 \ir ddl/regprc-transaction_type.sql
 \ir ddl/regprc-rid_seq.sql
 \ir ddl/regprc-reg_bio_ref.sql
