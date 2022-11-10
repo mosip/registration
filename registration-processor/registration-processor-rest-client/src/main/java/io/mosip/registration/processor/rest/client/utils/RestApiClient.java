bd88a976edb5443344494b5d630a8a2d79c104c9
Shubham Jaiswal <shubh.55jwl@gmail.com>
2022-11-10 18:33:10 +0530

    MOSIP-24572 I fixed vulnerability by excluded MOSIPActiveMq.java from registration pom.xml file

--- registration-processor/registration-processor-rest-client/src/main/java/io/mosip/registration/processor/rest/client/utils/RestApiClient.java
+++ registration-processor/registration-processor-rest-client/src/main/java/io/mosip/registration/processor/rest/client/utils/RestApiClient.java
@@ -64,7 +64,6 @@ public class RestApiClient {
 
 	private static final String AUTHORIZATION = "Authorization=";
 	
-	private static final String MOSIP_PUBLIC_URL = "mosipbox.public.url";
 
 	@Autowired
 	@Qualifier("selfTokenRestTemplate")
@@ -86,11 +85,6 @@ public class RestApiClient {
 	public <T> T getApi(URI uri, Class<?> responseType) throws Exception {
 		T result = null;
 		
-		String whiteListUrl=environment.getProperty(MOSIP_PUBLIC_URL);
-        String host = uri.getHost();
-        if(!whiteListUrl.contains(host)) {
-      	  throw new IOException();
-        }
 		try {
 			result = (T) localRestTemplate.exchange(uri, HttpMethod.GET, setRequestHeader(null, null), responseType)
 					.getBody();
