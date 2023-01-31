f92fc5e8238acf6355c78f9d03700d054895e0b2
Shubham Jaiswal <shubh.55jwl@gmail.com>
2023-01-31 20:33:54 +0530

    MOSIP-25748 done changes as per suggested by vishwa

--- registration-processor/core-processor/registration-processor-uin-generator-stage/src/main/java/io/mosip/registration/processor/stages/uingenerator/stage/UinGeneratorStage.java
+++ registration-processor/core-processor/registration-processor-uin-generator-stage/src/main/java/io/mosip/registration/processor/stages/uingenerator/stage/UinGeneratorStage.java
@@ -419,8 +419,8 @@ public class UinGeneratorStage extends MosipVerticleAPIManager {
 			InternalRegistrationStatusDto registrationStatusDto, JSONObject demographicIdentity,
 			boolean isUinAlreadyPresent) throws ApisResourceAccessException, IOException, Exception,
 			VidCreationException, VidServiceFailedException {
-		sendResponseToUinGenerator(registrationId, uinResponseDto.getResponse().getUin(),
-				UINConstants.UIN_UNASSIGNED);
+		//sendResponseToUinGenerator(registrationId, uinResponseDto.getResponse().getUin(),
+				//UINConstants.UIN_UNASSIGNED);
 		String uin = idRepoService.getUinByRid(registrationId,
 				utility.getGetRegProcessorDemographicIdentity());
 
@@ -544,7 +544,7 @@ private void handleIdRepoSuccessResponse(InternalRegistrationStatusDto registrat
 	registrationStatusDto.setStatusComment(StatusUtil.UIN_GENERATED_SUCCESS.getMessage());
 	registrationStatusDto.setSubStatusCode(StatusUtil.UIN_GENERATED_SUCCESS.getCode());
 	String uinStatus = isUinAlreadyPresent ? UINConstants.UIN_UNASSIGNED : UINConstants.UIN_ASSIGNED;
-	//sendResponseToUinGenerator(registrationId, uinResponseDto.getResponse().getUin(), uinStatus);
+	sendResponseToUinGenerator(registrationId, uinResponseDto.getResponse().getUin(), uinStatus);
 	isTransactionSuccessful = true;
 	registrationStatusDto.setStatusCode(RegistrationStatusCode.PROCESSED.toString());
 	description.setMessage(PlatformSuccessMessages.RPR_UIN_GENERATOR_STAGE_SUCCESS.getMessage());
@@ -1095,13 +1095,6 @@ private void handleIdRepoErrorResponse(IdResponseDTO idResponseDTO, InternalRegi
 		mosipEventBus = this.getEventBus(this, clusterManagerUrl, workerPoolSize);
 		this.consumeAndSend(mosipEventBus, MessageBusAddress.UIN_GENERATION_BUS_IN,
 				MessageBusAddress.UIN_GENERATION_BUS_OUT, messageExpiryTimeLimit);
-		
-		MessageDTO msg=new MessageDTO();
-		msg.setIsValid(true);
-		msg.setInternalError(false);
-		msg.setReg_type(io.mosip.registration.processor.core.constant.RegistrationType.NEW);
-		msg.setRid("10002300071102820221214002007");
-		process(msg);
 	}
 
 	@Override
