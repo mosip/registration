5c912b6057b62bf17990dccba0d9d9437708d425
Sowmya Ujjappa Banakar <m1022006@mindtree.com>
2023-03-15 17:11:21 +0530

    MOSIP-26444 code review comment fixed

--- registration-processor/registration-processor-core/src/main/java/io/mosip/registration/processor/core/logger/RegProcessorLogger.java
+++ registration-processor/registration-processor-core/src/main/java/io/mosip/registration/processor/core/logger/RegProcessorLogger.java
@@ -3,9 +3,12 @@ package io.mosip.registration.processor.core.logger;
 import io.mosip.kernel.core.logger.spi.Logger;
 import org.slf4j.LoggerFactory;
 import io.mosip.kernel.logger.logback.factory.Logfactory;
-import java.util.Properties;
+
+
+
+import java.util.stream.Collectors;
 import java.util.Map;
-import java.util.HashMap;
+
 import ch.qos.logback.classic.Level;
 import ch.qos.logback.classic.LoggerContext;
 
@@ -16,7 +19,9 @@ import ch.qos.logback.classic.LoggerContext;
  */
 public final class RegProcessorLogger {
 	
-	public static final String PROP_PREFIX = "logging.level";
+	public static final String PROP_PREFIX = "logging.level.";
+	
+	public static Map<String,String> loggingLevelMap;
 	
 	/**
 	 * Instantiates a new reg processor logger.
@@ -31,20 +36,20 @@ public final class RegProcessorLogger {
 	 * @return the logger
 	 */
 	public static Logger getLogger(Class<?> clazz) {
-		Map<String,String> map = new HashMap<String, String>();
+		if(loggingLevelMap.isEmpty()) {
+			loggingLevelMap = System.getProperties().entrySet().stream()
+					.filter(entry -> entry.getKey().toString().startsWith(PROP_PREFIX))
+					.collect(Collectors.toMap(entry -> (String)entry.getKey(), entry -> (String)entry.getValue()));
+		}
 		Logger logger=  Logfactory.getSlf4jLogger(clazz);
-		if(map.isEmpty()) {
-			Properties properties=System.getProperties();
+		String loggerName = clazz.getName();
+		if(loggingLevelMap.entrySet().stream().anyMatch( entry -> entry.getKey().equals(PROP_PREFIX+loggerName))) {
 			LoggerContext loggerContext = (LoggerContext) LoggerFactory.getILoggerFactory();
-			properties.forEach((key, value) -> {
-			      if (key.toString().startsWith(PROP_PREFIX)) {
-			        map.put(key.toString().substring(PROP_PREFIX.length() + 1), value.toString());
-			      }
-			    });
-				map.forEach((key, value) -> {
-					loggerContext.getLogger(key).setLevel(Level.valueOf(value));
-				    });
+			ch.qos.logback.classic.Logger slf4jlogger=loggerContext.getLogger(loggerName);
+			if(slf4jlogger!=null) {
+			   slf4jlogger.setLevel(Level.valueOf(loggingLevelMap.get(PROP_PREFIX+loggerName)));
 			}
+		}
 		return logger;
 	}
 }
