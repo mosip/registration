cf384783c0a58f132539c4c7e9c347621bf2efc3
Sowmya Ujjappa Banakar <m1022006@mindtree.com>
2023-03-27 11:34:32 +0530

    MOSIP-23945 code review fix

--- registration-processor/registration-processor-message-sender-impl/src/main/java/io/mosip/registration/processor/message/sender/service/impl/MessageNotificationServiceImpl.java
+++ registration-processor/registration-processor-message-sender-impl/src/main/java/io/mosip/registration/processor/message/sender/service/impl/MessageNotificationServiceImpl.java
@@ -162,7 +162,9 @@ public class MessageNotificationServiceImpl
 	@Autowired
 	private ObjectMapper mapper;
 	
-	private JSONObject mapperJson = null;
+	private List<String> mapperJsonKeys = null;
+	
+	private JSONObject mapperIdentity=null;
 
 	/*
 	 * (non-Javadoc)
@@ -534,18 +536,15 @@ public class MessageNotificationServiceImpl
 	private Map<String, Object> setAttributes(String idJsonString, Map<String, Object> attribute, String regType,
 			String lang, StringBuilder phoneNumber, StringBuilder emailId) throws IOException {
 		JSONObject demographicIdentity = null;
-
 			demographicIdentity = JsonUtil.objectMapperReadValue(idJsonString, JSONObject.class);
 
-        if(mapperJson==null) {
+        if(mapperJsonKeys==null) {
         	String mapperJsonString = Utilities.getJson(utility.getConfigServerFileStorageURL(),
     				utility.getGetRegProcessorIdentityJson());
-    		 mapperJson = JsonUtil.objectMapperReadValue(mapperJsonString, JSONObject.class);
+        	JSONObject mapperJson = JsonUtil.objectMapperReadValue(mapperJsonString, JSONObject.class);
+		    mapperIdentity = JsonUtil.getJSONObject(mapperJson, utility.getGetRegProcessorDemographicIdentity());
+		   mapperJsonKeys = new ArrayList<>(mapperIdentity.keySet());
         }
-	
-		JSONObject mapperIdentity = JsonUtil.getJSONObject(mapperJson, utility.getGetRegProcessorDemographicIdentity());
-
-		List<String> mapperJsonKeys = new ArrayList<>(mapperIdentity.keySet());
 		for (String key : mapperJsonKeys) {
 			JSONObject jsonValue = JsonUtil.getJSONObject(mapperIdentity, key);
 			if (jsonValue.get(VALUE) != null && !jsonValue.get(VALUE).toString().isBlank()) {
