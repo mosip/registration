d990ec1b722e6cc162dc1ba34d0ba19d6b961330
Sowmya Ujjappa Banakar <43202851+sowmya695@users.noreply.github.com>
2023-05-09 15:48:20 +0530

    MOSIP-23715 Code changes,stage test class changed because of eventbus new methods added. (#1693)
    * MOSIP-23715 code changes
    * Added kafka test cases
    ---------
    
    Co-authored-by: M1022006 <M1022006@A2ML18485.mindtree.com>
    Co-authored-by: Sowmya Ujjappa Banakar <m1022006@mindtree.com>
    Signed-off-by: Sowmya Ujjappa Banakar <sowmya.61022006@ltimindtree.com>

--- registration-processor/pre-processor/registration-processor-packet-classifier-stage/src/test/java/io/mosip/registration/processor/stages/packetclassifier/PacketClassifierStageTest.java
+++ registration-processor/pre-processor/registration-processor-packet-classifier-stage/src/test/java/io/mosip/registration/processor/stages/packetclassifier/PacketClassifierStageTest.java
@@ -1,7 +1,7 @@
 package io.mosip.registration.processor.stages.packetclassifier;
 
 import static org.junit.Assert.assertTrue;
-import static org.mockito.Matchers.any;
+import static org.mockito.ArgumentMatchers.any;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -12,6 +12,7 @@ import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.test.util.ReflectionTestUtils;
 
 import io.mosip.registration.processor.core.abstractverticle.EventDTO;
+import io.mosip.registration.processor.core.abstractverticle.HealthCheckDTO;
 import io.mosip.registration.processor.core.abstractverticle.MessageBusAddress;
 import io.mosip.registration.processor.core.abstractverticle.MessageDTO;
 import io.mosip.registration.processor.core.abstractverticle.MosipEventBus;
@@ -60,6 +61,18 @@ public class PacketClassifierStageTest {
 
 				@Override
 				public void send(MessageBusAddress toAddress, MessageDTO message) {
+				}
+
+				@Override
+				public void consumerHealthCheck(Handler<HealthCheckDTO> eventHandler, String address) {
+					// TODO Auto-generated method stub
+
+				}
+
+				@Override
+				public void senderHealthCheck(Handler<HealthCheckDTO> eventHandler, String address) {
+					// TODO Auto-generated method stub
+
 				}
 			};
 		}
