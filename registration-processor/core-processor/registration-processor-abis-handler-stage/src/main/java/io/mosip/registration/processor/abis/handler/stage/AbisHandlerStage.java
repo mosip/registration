65f64fcedc91db0e879969ffedb75ab532cc4f6c
Sowmya Ujjappa Banakar <43202851+sowmya695@users.noreply.github.com>
2023-08-22 13:20:24 +0530

    MOSIP-28646 code changes (#1757)
    Co-authored-by: Sowmya Ujjappa Banakar <m1022006@mindtree.com>
    Signed-off-by: Sowmya Ujjappa Banakar <sowmya.61022006@ltimindtree.com>

--- registration-processor/core-processor/registration-processor-abis-handler-stage/src/main/java/io/mosip/registration/processor/abis/handler/stage/AbisHandlerStage.java
+++ registration-processor/core-processor/registration-processor-abis-handler-stage/src/main/java/io/mosip/registration/processor/abis/handler/stage/AbisHandlerStage.java
@@ -153,14 +153,8 @@ public class AbisHandlerStage extends MosipVerticleAPIManager {
 	@Value("${mosip.regproc.data.share.internal.domain.name}")
 	private String internalDomainName;
 
-	@Value("#{${mosip.regproc.abis.handler.biometric-modalities-segments-mapping.INFANT}}")
-	private Map<String, List<String>> biometricModalitySegmentsMapInfant;
-
-	@Value("#{${mosip.regproc.abis.handler.biometric-modalities-segments-mapping.MINOR}}")
-	private Map<String, List<String>> biometricModalitySegmentsMapMinor;
-
-	@Value("#{${mosip.regproc.abis.handler.biometric-modalities-segments-mapping.ADULT}}")
-	private Map<String, List<String>> biometricModalitySegmentsMapAdult;
+	@Value("#{${mosip.regproc.abis.handler.biometric-modalities-segments-mapping-for-age-group}}")
+	private Map<String, Map<String, List<String>>> biometricModalitySegmentsMapforAgeGroup;
 
 	@Value("#{${mosip.regproc.abis.handler.biometric-segments-exceptions-mapping}}")
 	private Map<String, String> exceptionSegmentsMap;
@@ -600,20 +594,15 @@ public class AbisHandlerStage extends MosipVerticleAPIManager {
 
 		Map<String, String> tags = packetManagerService.getAllTags(id);
 		String ageGroup = tags.get("AGE_GROUP");
-
-		if (ageGroup.equalsIgnoreCase("INFANT")) {
-			validateBiometricRecord(biometricRecord, modalities, biometricModalitySegmentsMapInfant,
-					priorityBasedPacketManagerService.getMetaInfo(id, process, ProviderStageName.BIO_DEDUPE));
-		} else if (ageGroup.equalsIgnoreCase("MINOR")) {
-			validateBiometricRecord(biometricRecord, modalities, biometricModalitySegmentsMapMinor,
-					priorityBasedPacketManagerService.getMetaInfo(id, process, ProviderStageName.BIO_DEDUPE));
-		} else if (ageGroup.equalsIgnoreCase("ADULT")) {
-			validateBiometricRecord(biometricRecord, modalities, biometricModalitySegmentsMapAdult,
-					priorityBasedPacketManagerService.getMetaInfo(id, process, ProviderStageName.BIO_DEDUPE));
-		} else {
-			validateBiometricRecord(biometricRecord, modalities, biometricModalitySegmentsMapAdult,
-					priorityBasedPacketManagerService.getMetaInfo(id, process, ProviderStageName.BIO_DEDUPE));
+		Map<String, List<String>> biometricModalitySegmentsMap;
+		if(biometricModalitySegmentsMapforAgeGroup.containsKey(ageGroup)){
+			biometricModalitySegmentsMap = biometricModalitySegmentsMapforAgeGroup.get(ageGroup);
+			}
+		else {
+			biometricModalitySegmentsMap = biometricModalitySegmentsMapforAgeGroup.get("DEFAULT");
 		}
+		validateBiometricRecord(biometricRecord, modalities, biometricModalitySegmentsMap,
+					priorityBasedPacketManagerService.getMetaInfo(id, process, ProviderStageName.BIO_DEDUPE));
 
 		byte[] content = cbeffutil.createXML(filterExceptionBiometrics(biometricRecord,id,process).getSegments());
 
