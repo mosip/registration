af940df17ae9a35feca5a0eef06ac662b93f2b2b
Muralitharan K <muralitharan@mosip.io>
2023-08-23 14:24:00 +0530

    DST-118: Fix for update flow and code refactor

--- registration-processor/post-processor/registration-processor-printing-stage/src/main/java/io/mosip/registration/processor/print/util/CredentialPartnerUtil.java
+++ registration-processor/post-processor/registration-processor-printing-stage/src/main/java/io/mosip/registration/processor/print/util/CredentialPartnerUtil.java
@@ -3,7 +3,6 @@ package io.mosip.registration.processor.print.util;
 import io.mosip.kernel.core.exception.BaseCheckedException;
 import io.mosip.kernel.core.exception.ExceptionUtils;
 import io.mosip.kernel.core.logger.spi.Logger;
-import io.mosip.kernel.core.util.exception.JsonProcessingException;
 import io.mosip.registration.processor.core.constant.LoggerFileConstant;
 import io.mosip.registration.processor.core.constant.MappingJsonConstants;
 import io.mosip.registration.processor.core.constant.ProviderStageName;
@@ -76,7 +75,7 @@ public class CredentialPartnerUtil {
             List<String> keys = identityFieldValueMap.entrySet().stream().filter(e -> ObjectUtils.isEmpty(e.getValue())).map(Map.Entry::getKey).collect(Collectors.toList());
             if (keys != null && !keys.isEmpty()) {
                 JSONObject jsonObject = utilities.retrieveIdrepoJson(uin);
-                keys.forEach(key -> identityFieldValueMap.put(key, (String) jsonObject.get(key)));
+                keys.forEach(key -> identityFieldValueMap.put(key, jsonObject.get(key) != null ? jsonObject.get(key).toString() : null));
             }
         }
 
