2eb013b40c16b3834247bef5ffbbbe45915bc12c
Monobikash Das <monobikash.das@mindtree.com>
2023-11-08 12:23:42 +0530

    MOSIP-28121 : added partner profile feature
    Signed-off-by: Monobikash Das <monobikash.das@mindtree.com>

--- registration-processor/post-processor/registration-processor-credential-requestor-stage/src/main/java/io/mosip/registration/processor/credentialrequestor/stage/CredentialRequestorStage.java
+++ registration-processor/post-processor/registration-processor-credential-requestor-stage/src/main/java/io/mosip/registration/processor/credentialrequestor/stage/CredentialRequestorStage.java
@@ -1,16 +1,17 @@
 package io.mosip.registration.processor.credentialrequestor.stage;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import io.mosip.kernel.core.exception.BaseUncheckedException;
 import io.mosip.kernel.core.exception.ServiceError;
 import io.mosip.kernel.core.logger.spi.Logger;
 import io.mosip.kernel.core.util.DateUtils;
 import io.mosip.registration.processor.core.abstractverticle.*;
 import io.mosip.registration.processor.core.code.*;
+import io.mosip.registration.processor.core.code.EventId;
+import io.mosip.registration.processor.core.code.EventName;
+import io.mosip.registration.processor.core.code.EventType;
 import io.mosip.registration.processor.core.common.rest.dto.ErrorDTO;
-import io.mosip.registration.processor.core.constant.IdType;
-import io.mosip.registration.processor.core.constant.LoggerFileConstant;
-import io.mosip.registration.processor.core.constant.ProviderStageName;
-import io.mosip.registration.processor.core.constant.VidType;
+import io.mosip.registration.processor.core.constant.*;
 import io.mosip.registration.processor.core.exception.ApisResourceAccessException;
 import io.mosip.registration.processor.core.exception.util.PlatformErrorMessages;
 import io.mosip.registration.processor.core.exception.util.PlatformSuccessMessages;
@@ -26,15 +27,17 @@ import io.mosip.registration.processor.core.spi.restclient.RegistrationProcessor
 import io.mosip.registration.processor.core.status.util.StatusUtil;
 import io.mosip.registration.processor.core.status.util.TrimExceptionMessage;
 import io.mosip.registration.processor.core.util.JsonUtil;
-import io.mosip.registration.processor.packet.storage.exception.ParsingException;
-import io.mosip.registration.processor.packet.storage.utils.Utilities;
+import io.mosip.registration.processor.credentialrequestor.dto.CredentialPartner;
 import io.mosip.registration.processor.credentialrequestor.stage.exception.VidNotAvailableException;
 import io.mosip.registration.processor.credentialrequestor.util.CredentialPartnerUtil;
+import io.mosip.registration.processor.packet.storage.exception.ParsingException;
+import io.mosip.registration.processor.packet.storage.utils.Utilities;
 import io.mosip.registration.processor.rest.client.audit.builder.AuditLogRequestBuilder;
 import io.mosip.registration.processor.status.code.RegistrationStatusCode;
 import io.mosip.registration.processor.status.dto.InternalRegistrationStatusDto;
 import io.mosip.registration.processor.status.dto.RegistrationStatusDto;
 import io.mosip.registration.processor.status.service.RegistrationStatusService;
+import org.apache.commons.collections.CollectionUtils;
 import org.apache.commons.lang.StringUtils;
 import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.json.JSONArray;
@@ -51,13 +54,13 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
-import java.io.File;
 import java.io.IOException;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.*;
+import java.util.stream.Collectors;
 
 /**
  * The Class PrintStage.
@@ -87,10 +90,6 @@ public class CredentialRequestorStage extends MosipVerticleAPIManager {
 	private Random sr = null;
 	private static final int max = 999999;
 	private static final int min = 100000;
-
-	/** The Constant FILE_SEPARATOR. */
-	public static final String FILE_SEPARATOR = File.separator;
-
 
 	/** The reg proc logger. */
 	private static Logger regProcLogger = RegProcessorLogger.getLogger(CredentialRequestorStage.class);
@@ -122,14 +121,9 @@ public class CredentialRequestorStage extends MosipVerticleAPIManager {
 	@Value("${mosip.registration.processor.encrypt:false}")
 	private boolean encrypt;
 
-	@Value("#{${mosip.registration.processor.print.issuer.config-map:{}}}")
-	private Map<String, String> credentialPartnerMap;
-
 	/** Mosip router for APIs */
 	@Autowired
 	MosipRouter router;
-
-	boolean isConnection = false;
 
 	private static final String SEPERATOR = "::";
 
@@ -146,17 +140,8 @@ public class CredentialRequestorStage extends MosipVerticleAPIManager {
 
 	private static final String ISSUERS = "mosip.registration.processor.issuer";
 
-	@Value("${mosip.registration.processor.rid.delimiter:-PDF}")
-	private String pdfDelimiter;
-
-	@Value("#{T(java.util.Arrays).asList('${mosip.registration.processor.default.internal.issuers:}')}")
-	private List<String> defaultInternalIssuers;
-
-	@Value("#{T(java.util.Arrays).asList('${mosip.registration.processor.default.issuers:}')}")
-	private List<String> defaultIssuers;
-
-	@Value("${mosip.registration.processor.additional.credential.request:}")
-	private String additionalConfiguredCredentials;
+	@Value("#{T(java.util.Arrays).asList('${mosip.registration.processor.credential.default.partner-ids:}')}")
+	private List<String> defaultPartners;
 
 	private static String COMMA = ",";
 	private static String HASH_DELIMITER = "#";
@@ -198,6 +183,7 @@ public class CredentialRequestorStage extends MosipVerticleAPIManager {
 
 		boolean isTransactionSuccessful = false;
 		String uin = null;
+		String refIds = null;
 		String regId = object.getRid();
 		regProcLogger.debug(LoggerFileConstant.SESSIONID.toString(), LoggerFileConstant.REGISTRATIONID.toString(),
 				regId, "PrintStage::process()::entry");
@@ -232,56 +218,59 @@ public class CredentialRequestorStage extends MosipVerticleAPIManager {
 						.setLatestTransactionTypeCode(RegistrationTransactionTypeCode.PRINT_SERVICE.toString());
 
 			} else {
-				String vid = getVid(uin);
 				requestWrapper.setId(env.getProperty("mosip.registration.processor.credential.request.service.id"));
 				DateTimeFormatter format = DateTimeFormatter.ofPattern(env.getProperty(DATETIME_PATTERN));
 				requestWrapper.setVersion("1.0");
-				Set<String> credentialIssuerSet = new HashSet<>();
-				credentialIssuerSet.addAll(defaultInternalIssuers);
-				credentialIssuerSet.addAll(defaultIssuers);
-				if (credentialPartnerMap != null && !credentialPartnerMap.isEmpty()) {
-					credentialIssuerSet.addAll(credentialPartnerUtil.getCredentialPartners(
-							regId, registrationStatusDto.getRegistrationType(), jsonObject));
-				}
-				for (String key : credentialIssuerSet) {
-					String[] parts = key.split(HASH_DELIMITER, 3);
-					CredentialRequestDto credentialRequestDto = getCredentialRequestDto(regId, registrationStatusDto.getRegistrationType(), parts[0], parts[1], parts[2]);
+				List<CredentialPartner> allIssuerList = credentialPartnerUtil.getAllCredentialPartners().getPartners();
+				// filtering with default partner ids and process
+				List<CredentialPartner> filteredPartners = allIssuerList.stream()
+						.filter(issuer -> defaultPartners.contains(issuer.getId()))
+						.filter(issuer -> (issuer.getProcess() == null) || (issuer.getProcess().contains(object.getReg_type())))
+						.collect(Collectors.toList());
+				filteredPartners.addAll(credentialPartnerUtil.getCredentialPartners(
+						regId, registrationStatusDto.getRegistrationType(), jsonObject));
+				for (CredentialPartner key : filteredPartners) {
+					CredentialRequestDto credentialRequestDto = getCredentialRequestDto(regId, registrationStatusDto.getRegistrationType(), key);
 					LocalDateTime localdatetime = LocalDateTime.parse(
 							DateUtils.getUTCCurrentDateTimeString(env.getProperty(DATETIME_PATTERN)), format);
 					requestWrapper.setRequesttime(localdatetime);
 					requestWrapper.setRequest(credentialRequestDto);
-					if (defaultInternalIssuers.contains(key)) {
+					// issuers with appIdBasedCredentialIdSuffix is calling v1 api and for others stage is calling v2 api for credential
+					if (StringUtils.isNotEmpty(key.getAppIdBasedCredentialIdSuffix())) {
 						List<String> pathsegments = new ArrayList<>();
-						pathsegments.add(regId + pdfDelimiter); //  #PDF suffix is added to identify the requested credential via rid
+						pathsegments.add(regId + key.getAppIdBasedCredentialIdSuffix()); //  #PDF suffix is added to identify the requested credential via rid
 						responseWrapper = (ResponseWrapper<?>) restClientService.postApi(ApiName.CREDENTIALREQUESTV2, MediaType.APPLICATION_JSON, pathsegments, null,
 									null, requestWrapper, ResponseWrapper.class);
-					} else { // for default issuer stage is calling v1 api and for others stage is calling v2 api for credential
+					} else {
 						responseWrapper = (ResponseWrapper<?>) restClientService.postApi(ApiName.CREDENTIALREQUEST, null, null,
 								requestWrapper, ResponseWrapper.class, MediaType.APPLICATION_JSON);
 					}
-				}
+					if (responseWrapper.getErrors() != null && !responseWrapper.getErrors().isEmpty()) {
+						ErrorDTO error = responseWrapper.getErrors().get(0);
+						object.setIsValid(Boolean.FALSE);
+						isTransactionSuccessful = false;
+						registrationStatusDto.setRefId(refIds);
+						description.setMessage(PlatformErrorMessages.RPR_PRT_PRINT_REQUEST_FAILED.getMessage());
+						description.setCode(PlatformErrorMessages.RPR_PRT_PRINT_REQUEST_FAILED.getCode());
 
-				if (responseWrapper.getErrors() != null && !responseWrapper.getErrors().isEmpty()) {
-					ErrorDTO error = responseWrapper.getErrors().get(0);
-					object.setIsValid(Boolean.FALSE);
-					isTransactionSuccessful = false;
-					description.setMessage(PlatformErrorMessages.RPR_PRT_PRINT_REQUEST_FAILED.getMessage());
-					description.setCode(PlatformErrorMessages.RPR_PRT_PRINT_REQUEST_FAILED.getCode());
-
-					registrationStatusDto.setStatusComment(
-							StatusUtil.PRINT_REQUEST_FAILED.getMessage() + SEPERATOR + error.getMessage());
-					registrationStatusDto.setSubStatusCode(StatusUtil.PRINT_REQUEST_FAILED.getCode());
-					registrationStatusDto
+						registrationStatusDto.setStatusComment(
+								StatusUtil.PRINT_REQUEST_FAILED.getMessage() + SEPERATOR + error.getMessage());
+						registrationStatusDto.setSubStatusCode(StatusUtil.PRINT_REQUEST_FAILED.getCode());
+						registrationStatusDto
 								.setLatestTransactionStatusCode(RegistrationTransactionStatusCode.REPROCESS.toString());
-					registrationStatusDto
-							.setLatestTransactionTypeCode(RegistrationTransactionTypeCode.PRINT_SERVICE.toString());
-				} else {
-					credentialResponseDto = mapper.readValue(mapper.writeValueAsString(responseWrapper.getResponse()),
-							CredentialResponseDto.class);
-
-					registrationStatusDto.setRefId(credentialResponseDto.getRequestId());
+						registrationStatusDto
+								.setLatestTransactionTypeCode(RegistrationTransactionTypeCode.PRINT_SERVICE.toString());
+						break;
+					} else {
+						credentialResponseDto = mapper.readValue(mapper.writeValueAsString(responseWrapper.getResponse()),
+								CredentialResponseDto.class);
+						refIds = credentialResponseDto.getRequestId();
+						isTransactionSuccessful = true;
+					}
+				}
+				if (isTransactionSuccessful) {
+					registrationStatusDto.setRefId(refIds);
 					object.setIsValid(Boolean.TRUE);
-					isTransactionSuccessful = true;
 					description.setMessage(PlatformSuccessMessages.RPR_PRINT_STAGE_REQUEST_SUCCESS.getMessage());
 					description.setCode(PlatformSuccessMessages.RPR_PRINT_STAGE_REQUEST_SUCCESS.getCode());
 					registrationStatusDto.setStatusComment(
@@ -317,18 +306,6 @@ public class CredentialRequestorStage extends MosipVerticleAPIManager {
 			registrationStatusDto.setStatusComment(
 					trimeExpMessage.trimExceptionMessage(StatusUtil.IO_EXCEPTION.getMessage() + e.getMessage()));
 			registrationStatusDto.setSubStatusCode(StatusUtil.IO_EXCEPTION.getCode());
-			description.setMessage(PlatformErrorMessages.RPR_PRT_PRINT_REQUEST_FAILED.getMessage());
-			description.setCode(PlatformErrorMessages.RPR_PRT_PRINT_REQUEST_FAILED.getCode());
-			object.setInternalError(Boolean.TRUE);
-		} catch (VidNotAvailableException e) {
-			regProcLogger.error(LoggerFileConstant.SESSIONID.toString(), LoggerFileConstant.REGISTRATIONID.toString(),
-					regId, PlatformErrorMessages.RPR_PRT_PRINT_REQUEST_FAILED.name() + e.getMessage()
-							+ ExceptionUtils.getStackTrace(e));
-			registrationStatusDto
-					.setLatestTransactionStatusCode(RegistrationTransactionStatusCode.REPROCESS.toString());
-			registrationStatusDto.setStatusComment(
-					trimeExpMessage.trimExceptionMessage(StatusUtil.VID_NOT_AVAILABLE.getMessage()));
-			registrationStatusDto.setSubStatusCode(StatusUtil.VID_NOT_AVAILABLE.getCode());
 			description.setMessage(PlatformErrorMessages.RPR_PRT_PRINT_REQUEST_FAILED.getMessage());
 			description.setCode(PlatformErrorMessages.RPR_PRT_PRINT_REQUEST_FAILED.getCode());
 			object.setInternalError(Boolean.TRUE);
@@ -371,70 +348,46 @@ public class CredentialRequestorStage extends MosipVerticleAPIManager {
 		return object;
 	}
 
-	private CredentialRequestDto getCredentialRequestDto(String regId, String process, String issuerId,String credentialType,String templateTypeCode) {
+	private CredentialRequestDto getCredentialRequestDto(String regId, String process, CredentialPartner key) {
 		CredentialRequestDto credentialRequestDto = new CredentialRequestDto();
 		Map<String, Object> additionalAttributes=new HashMap<>();
 
-		credentialRequestDto.setCredentialType(credentialType);
+		credentialRequestDto.setCredentialType(key.getCredentialType());
 		credentialRequestDto.setEncrypt(encrypt);
 
 		credentialRequestDto.setId(regId);
 
-		credentialRequestDto.setIssuer(issuerId);
+		credentialRequestDto.setIssuer(key.getPartnerId());
 
 		credentialRequestDto.setEncryptionKey(generatePin());
-		additionalAttributes.put("templateTypeCode",templateTypeCode);
+		additionalAttributes.put("templateTypeCode", key.getTemplate());
 		additionalAttributes.put("registrationId", regId);
-		getAdditionalCredentialFields(regId, process, issuerId, credentialType, additionalAttributes);
+		if (CollectionUtils.isNotEmpty(key.getMetaInfoFields()))
+			getAdditionalCredentialFields(regId, process, key.getMetaInfoFields(), additionalAttributes);
 		credentialRequestDto.setAdditionalData(additionalAttributes);
 
 		return credentialRequestDto;
 	}
 
-	private void getAdditionalCredentialFields(String regId, String process, String issuerId,
-											   String credentialType, Map<String, Object> additionalAttributes) {
-		if (StringUtils.isEmpty(additionalConfiguredCredentials))
-			return;
-		try {
-			JSONArray jArray=new JSONArray(additionalConfiguredCredentials);
-			for(int i=0;i<jArray.length();i++) {
-				if (process.equalsIgnoreCase(jArray.getJSONObject(i).getString("process"))
-				&& credentialType.equalsIgnoreCase(jArray.getJSONObject(i).getString("credentialType"))
-				&& issuerId.equalsIgnoreCase(jArray.getJSONObject(i).getString("issuer"))) {
-					if (jArray.getJSONObject(i).has("additionalMetaInfoField")) {
-						JSONArray jArray2 = jArray.getJSONObject(i).getJSONArray("additionalFieldsInCredential");
-						for(int j=0;j<jArray2.length();j++) {
-							String additionalAttr = jArray2.get(j).toString();
-							String additionalAttrValue = getCrvsField(regId, process, additionalAttr);
-							additionalAttributes.put(additionalAttr, additionalAttrValue);
-						}
-					}
-				}
-			}
-		} catch (JSONException e) {
-			regProcLogger.error(LoggerFileConstant.SESSIONID.toString(), LoggerFileConstant.REGISTRATIONID.toString(),
-					regId, RegistrationStatusCode.FAILED + e.getMessage() + ExceptionUtils.getStackTrace(e));
-			throw new ParsingException(
-					PlatformErrorMessages.RPR_PRT_PARSING_ADDITIONAL_CRED_CONFIG.getMessage(), e);
-		}
-	}
-
-	private String getCrvsField(String regId, String process, String additionalAttr){
+	private void getAdditionalCredentialFields(String regId, String process,
+											   List<String> metaInfoFields,
+											   Map<String, Object> additionalAttributes) {
 		try {
 			Map<String,String> metaInfo = utilities.getPacketManagerService().getMetaInfo(regId, process, ProviderStageName.CREDENTIAL_REQUESTOR);
-			JSONArray metadata = new JSONArray(metaInfo.get("metaData"));
-			for(int i=0;i<metadata.length();i++){
-				if(metadata.getJSONObject(i).getString("label").equalsIgnoreCase(additionalAttr)){
-					regProcLogger.debug(LoggerFileConstant.SESSIONID.toString(), LoggerFileConstant.REGISTRATIONID.toString(), regId, additionalAttr + " obtained.");
-					return metadata.getJSONObject(i).getString("value");
+			JSONArray metadata = new JSONArray(metaInfo.get(JsonConstant.METADATA));
+			for(int i=0; i<metadata.length(); i++){
+				org.json.JSONObject jsonObject = metadata.getJSONObject(i);
+				if (metaInfoFields.contains(jsonObject.getString("label"))){
+					additionalAttributes.put(jsonObject.getString("label"), jsonObject.getString("value"));
 				}
 			}
-		} catch (Exception e){
-			regProcLogger.warn(LoggerFileConstant.SESSIONID.toString(), LoggerFileConstant.REGISTRATIONID.toString(), regId, "Failed to get : " + additionalAttr + org.apache.commons.lang3.exception.ExceptionUtils.getStackTrace(e));
-			return null;
+
+		} catch (Exception e) {
+			regProcLogger.error(LoggerFileConstant.SESSIONID.toString(), LoggerFileConstant.REGISTRATIONID.toString(),
+					regId, RegistrationStatusCode.FAILED + e.getMessage() + ExceptionUtils.getStackTrace(e));
+			throw new BaseUncheckedException(PlatformErrorMessages.RPR_PRT_PARSING_ADDITIONAL_CRED_CONFIG.getCode(),
+					PlatformErrorMessages.RPR_PRT_PARSING_ADDITIONAL_CRED_CONFIG.getMessage(), e);
 		}
-		regProcLogger.warn(LoggerFileConstant.SESSIONID.toString(), LoggerFileConstant.REGISTRATIONID.toString(), regId, additionalAttr + " Not Found.");
-		return null;
 	}
 
 
