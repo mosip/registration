4a7124aed3aa726ef397bfd9d2f214cc8d8bb7ec
khuddus shariff <khuddusshariff0022@gmail.com>
2023-12-07 16:04:16 +0530

    [mosip 30470] uin genration Invalid input parameter Exception handeled
    Signed-off-by: khuddus shariff <khuddusshariff0022@gmail.com>

--- registration-processor/core-processor/registration-processor-uin-generator-stage/src/main/java/io/mosip/registration/processor/stages/uingenerator/stage/UinGeneratorStage.java
+++ registration-processor/core-processor/registration-processor-uin-generator-stage/src/main/java/io/mosip/registration/processor/stages/uingenerator/stage/UinGeneratorStage.java
@@ -280,6 +280,7 @@ public class UinGeneratorStage extends MosipVerticleAPIManager {
 							uinField, description);
 					if(idResponseDTO.getErrors()!=null && idResponseDTO.getErrors().get(0).getErrorCode().equalsIgnoreCase(INVALID_INPUT_PARAMETER_ERROR_CODE)) {
 						for (int i = 0; i < maxRetrycount; i++) {
+							regProcLogger.info(StatusUtil.INVALID_INPUT_PARAMETER.getMessage()+":"+StatusUtil.INVALID_INPUT_PARAMETER.getCode()+", Retrying "+(i+1)+" times");
 								idResponseDTO = sendIdRepoWithUin(registrationId, registrationStatusDto.getRegistrationType(), demographicIdentity,
 										uinField, description);
 								if (idResponseDTO.getErrors()==null || idResponseDTO.getErrors().get(0).getErrorCode()!=INVALID_INPUT_PARAMETER_ERROR_CODE)
