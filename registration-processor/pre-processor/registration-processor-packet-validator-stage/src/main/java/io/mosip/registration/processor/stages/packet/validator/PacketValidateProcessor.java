ea41e99712b3e1986ff47e52259338c14c36f0cb
khuddus shariff <khuddusshariff0022@gmail.com>
2024-01-09 13:07:33 +0530

    [Mosip_29895]cherry pick develop from mosip_29896
    Signed-off-by: khuddus shariff <khuddusshariff0022@gmail.com>

--- registration-processor/pre-processor/registration-processor-packet-validator-stage/src/main/java/io/mosip/registration/processor/stages/packet/validator/PacketValidateProcessor.java
+++ registration-processor/pre-processor/registration-processor-packet-validator-stage/src/main/java/io/mosip/registration/processor/stages/packet/validator/PacketValidateProcessor.java
@@ -514,12 +514,12 @@ public class PacketValidateProcessor {
 				}
 			}
 			if (preRegId == null || preRegId.trim().isEmpty()) {
-				regProcLogger.info(LoggerFileConstant.REGISTRATIONID.toString(), registrationId,
+				regProcLogger.info(LoggerFileConstant.REGISTRATIONID.toString(), id,
 						"Pre-registration id not present.",
 						"Reverse datasync is not applicable for the registration id");
 				return;
 			}
-			if (registrationId != null) {
+			if (id != null) {
 				packetValidationDto.setTransactionSuccessful(false);
 				MainResponseDTO<ReverseDatasyncReponseDTO> mainResponseDto = null;
 				if (preRegId != null && !preRegId.trim().isEmpty()) {
@@ -531,14 +531,14 @@ public class PacketValidateProcessor {
 					reverseDataSyncRequestDto.setPreRegistrationIds(Arrays.asList(preRegId));
 					mainRequestDto.setRequest(reverseDataSyncRequestDto);
 					regProcLogger.debug(LoggerFileConstant.SESSIONID.toString(),
-							LoggerFileConstant.REGISTRATIONID.toString(), registrationId,
+							LoggerFileConstant.REGISTRATIONID.toString(), id,
 							"PacketValidateProcessor::reverseDataSync()::ReverseDataSync Api call started with request data :"
 									+ JsonUtil.objectMapperObjectToJson(mainRequestDto));
 					mainResponseDto = (MainResponseDTO) restClientService.postApi(ApiName.REVERSEDATASYNC, "", "",
 							mainRequestDto, MainResponseDTO.class);
 
 					regProcLogger.debug(LoggerFileConstant.SESSIONID.toString(),
-							LoggerFileConstant.REGISTRATIONID.toString(), registrationId,
+							LoggerFileConstant.REGISTRATIONID.toString(), id,
 							"\"PacketValidateProcessor::reverseDataSync()::ReverseDataSync Api call ended with response data : "
 									+ JsonUtil.objectMapperObjectToJson(mainResponseDto));
 					packetValidationDto.setTransactionSuccessful(true);
@@ -546,7 +546,7 @@ public class PacketValidateProcessor {
 				}
 				if (mainResponseDto != null && mainResponseDto.getErrors() != null
 						&& mainResponseDto.getErrors().size() > 0) {
-					regProcLogger.error(LoggerFileConstant.REGISTRATIONID.toString(), registrationId,
+					regProcLogger.error(LoggerFileConstant.REGISTRATIONID.toString(), id,
 							PlatformErrorMessages.REVERSE_DATA_SYNC_FAILED.getMessage(),
 							mainResponseDto.getErrors().toString());
 					packetValidationDto.setTransactionSuccessful(false);
@@ -558,7 +558,7 @@ public class PacketValidateProcessor {
 							+ " null response from rest client ");
 				} else {
 					packetValidationDto.setTransactionSuccessful(true);
-					regProcLogger.info(LoggerFileConstant.REGISTRATIONID.toString(), registrationId,
+					regProcLogger.info(LoggerFileConstant.REGISTRATIONID.toString(), id,
 							PlatformSuccessMessages.REVERSE_DATA_SYNC_SUCCESS.getMessage(), "");
 				}
 
