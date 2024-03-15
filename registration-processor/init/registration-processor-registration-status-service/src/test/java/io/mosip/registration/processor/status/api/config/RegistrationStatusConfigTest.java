6fd06d6ec1cc0f0fe520217748da783e8556e57f
Sowmya Ujjappa Banakar <sowmya.61022006@ltimindtree.com>
2024-03-15 17:25:50 +0530

    Code review comments fixed
    Signed-off-by: Sowmya Ujjappa Banakar <sowmya.61022006@ltimindtree.com>
    Signed-off-by: Dhanendra Sahu <dhanendra.tech@gmail.com>

--- registration-processor/init/registration-processor-registration-status-service/src/test/java/io/mosip/registration/processor/status/api/config/RegistrationStatusConfigTest.java
+++ registration-processor/init/registration-processor-registration-status-service/src/test/java/io/mosip/registration/processor/status/api/config/RegistrationStatusConfigTest.java
@@ -1,23 +1,22 @@
 package io.mosip.registration.processor.status.api.config;
 
-import io.mosip.kernel.core.authmanager.authadapter.spi.VertxAuthenticationProvider;
-import io.mosip.registration.processor.status.dto.AuthorizedRolesDto;
-import io.vertx.core.Vertx;
-import io.vertx.core.http.HttpMethod;
-import io.vertx.core.http.HttpServer;
-import io.vertx.ext.web.Router;
-import io.vertx.ext.web.RoutingContext;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ComponentScan.Filter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
 
+import io.mosip.kernel.core.authmanager.authadapter.spi.VertxAuthenticationProvider;
 import io.mosip.registration.processor.core.config.CoreConfigBean;
 import io.mosip.registration.processor.rest.client.config.RestConfigBean;
 import io.mosip.registration.processor.status.config.RegistrationStatusBeanConfig;
 import io.mosip.registration.processor.status.config.RegistrationStatusServiceBeanConfig;
+import io.mosip.registration.processor.status.dto.AuthorizedRolesDto;
+import io.vertx.core.Vertx;
+import io.vertx.core.http.HttpMethod;
+import io.vertx.core.http.HttpServer;
+import io.vertx.ext.web.Router;
+import io.vertx.ext.web.RoutingContext;
 
 @Configuration
 @ComponentScan(basePackages = {
