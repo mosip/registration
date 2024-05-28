805b36b6be03302bdf71ce45a8a6df2734cf4c46
Sowmya Ujjappa Banakar <43202851+sowmya695@users.noreply.github.com>
2024-05-28 10:52:22 +0530

    Mosip 32461 (#1873)
    * MOSIP-32461 intial changes
    
    Signed-off-by: Sowmya Ujjappa Banakar <sowmya.61022006@ltimindtree.com>
    
    * MOSIP-32461 few test cases fixed
    
    Signed-off-by: Sowmya Ujjappa Banakar <sowmya.61022006@ltimindtree.com>
    
    * MOSIP-32461 code fix
    
    Signed-off-by: Sowmya Ujjappa Banakar <sowmya.61022006@ltimindtree.com>
    
    * MOSIP-32461 code changes
    
    Signed-off-by: Sowmya Ujjappa Banakar <sowmya.61022006@ltimindtree.com>
    
    * MOSIP-32461 code fix
    
    Signed-off-by: Sowmya Ujjappa Banakar <sowmya.61022006@ltimindtree.com>
    
    * MOSIP-32461 code fix
    
    Signed-off-by: Sowmya Ujjappa Banakar <sowmya.61022006@ltimindtree.com>
    
    * MOSIP-32461 code fix
    
    Signed-off-by: Sowmya Ujjappa Banakar <sowmya.61022006@ltimindtree.com>
    
    * MOSIP-32461 code fix
    
    Signed-off-by: Sowmya Ujjappa Banakar <sowmya.61022006@ltimindtree.com>
    
    * MOSIP-32461 code changes
    
    Signed-off-by: Sowmya Ujjappa Banakar <sowmya.61022006@ltimindtree.com>
    
    * MOSIP-32461 code changes
    
    Signed-off-by: Sowmya Ujjappa Banakar <sowmya.61022006@ltimindtree.com>
    
    * MOSIP-32461 code changes
    
    Signed-off-by: Sowmya Ujjappa Banakar <sowmya.61022006@ltimindtree.com>
    
    * MOSIP-32461 docker changes
    
    Signed-off-by: Sowmya Ujjappa Banakar <sowmya.61022006@ltimindtree.com>
    
    * MOSIP-32461 docker changes
    
    Signed-off-by: Sowmya Ujjappa Banakar <sowmya.61022006@ltimindtree.com>
    
    * MOSIP-32461 docker changes
    
    Signed-off-by: Sowmya Ujjappa Banakar <sowmya.61022006@ltimindtree.com>
    
    * MOSIP-32461 docker changes
    
    Signed-off-by: Sowmya Ujjappa Banakar <sowmya.61022006@ltimindtree.com>
    
    * MOSIP-32461 activemq exclude from spring boot
    
    Signed-off-by: Sowmya Ujjappa Banakar <sowmya.61022006@ltimindtree.com>
    
    * MOSIP-32461 activemq exclude from spring boot
    
    Signed-off-by: Sowmya Ujjappa Banakar <sowmya.61022006@ltimindtree.com>
    
    * MOSIP-32461 code changes
    
    Signed-off-by: Sowmya Ujjappa Banakar <sowmya.61022006@ltimindtree.com>
    
    ---------
    
    Signed-off-by: Sowmya Ujjappa Banakar <sowmya.61022006@ltimindtree.com>
    Co-authored-by: Sowmya Ujjappa Banakar <sowmya.61022006@ltimindtree.com>

--- registration-processor/post-processor/registration-processor-registration-transaction-service/src/main/java/io/mosip/registration/processor/transaction/api/RegistrationTransactionServiceApplication.java
+++ registration-processor/post-processor/registration-processor-registration-transaction-service/src/main/java/io/mosip/registration/processor/transaction/api/RegistrationTransactionServiceApplication.java
@@ -2,6 +2,8 @@ package io.mosip.registration.processor.transaction.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jms.JmsAutoConfiguration;
+import org.springframework.boot.autoconfigure.jms.activemq.ActiveMQAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ComponentScan.Filter;
 import org.springframework.context.annotation.FilterType;
@@ -11,7 +13,7 @@ import io.mosip.registration.processor.core.config.configserverloader.PropertyLo
 import io.mosip.registration.processor.rest.client.config.RestConfigBean;
 import io.mosip.registration.processor.status.config.RegistrationStatusBeanConfig;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { JmsAutoConfiguration.class, ActiveMQAutoConfiguration.class })
 @ComponentScan(basePackages= {"io.mosip.registration.processor.status.*","io.mosip.registration.processor.rest.client.*",
 		"io.mosip.registration.processor.core.token.*", "io.mosip.registration.processor.core.config",
 		"io.mosip.registration.processor.transaction.*", "${mosip.auth.adapter.impl.basepackage}",
--- registration-processor/registration-processor-notification-service/src/main/java/io/mosip/registration/processor/notification/NotificationApplication.java
+++ registration-processor/registration-processor-notification-service/src/main/java/io/mosip/registration/processor/notification/NotificationApplication.java
@@ -1,13 +1,17 @@
 package io.mosip.registration.processor.notification;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jms.JmsAutoConfiguration;
+import org.springframework.boot.autoconfigure.jms.activemq.ActiveMQAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.FilterType;
 import org.springframework.context.annotation.ComponentScan.Filter;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 import io.mosip.registration.processor.core.config.configserverloader.PropertyLoaderConfig;
+
 import io.mosip.registration.processor.rest.client.config.RestConfigBean;
 /**
  * Init class for Notification service.
@@ -16,7 +20,7 @@ import io.mosip.registration.processor.rest.client.config.RestConfigBean;
  * @since 1.0.0
  *
  */
-@SpringBootApplication
+@SpringBootApplication(exclude = { JmsAutoConfiguration.class, ActiveMQAutoConfiguration.class })
 @ComponentScan( basePackages = { "io.mosip.registration.processor.notification.*",
 		"io.mosip.kernel.websub.api.*","${mosip.auth.adapter.impl.basepackage}", "io.mosip.registration.processor.message.sender.config",
 		"io.mosip.registration.processor.rest.client.*", "io.mosip.registration.processor.packet.storage.config",
