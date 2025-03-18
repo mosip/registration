d6952fb90a2d3564394f67a0cbae0e9c133ad03c
khuddus shariff <khuddusshariff0022@gmail.com>
2025-03-19 01:38:52 +0530

    [MOSIP-39781] PR comment fix
    Signed-off-by: khuddus shariff <khuddusshariff0022@gmail.com>

--- registration-processor/registration-processor-core/src/main/java/io/mosip/registration/processor/core/workflow/dto/NotificationInfoDTO.java
+++ registration-processor/registration-processor-core/src/main/java/io/mosip/registration/processor/core/workflow/dto/NotificationInfoDTO.java
@@ -1,7 +1,10 @@
 package io.mosip.registration.processor.core.workflow.dto;
 
+import lombok.Data;
+
 import java.io.Serializable;
 
+@Data
 public class NotificationInfoDTO implements Serializable {
 
     /**
@@ -12,24 +15,4 @@ public class NotificationInfoDTO implements Serializable {
     private String name;
     private String phone;
     private String email;
-
-    public String getName() {
-        return name;
     }
-    public void setName(String name) {
-        this.name = name;
-    }
-    public String getPhone() {
-        return phone;
-    }
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-    public String getEmail() {
-        return email;
-    }
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-}
