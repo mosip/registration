c90d07bd043c477a9ae2bd0849f342b4e1e12a87
trialblazerseee <84778104+trialblazerseee@users.noreply.github.com>
2025-05-31 21:07:29 +0530

    MOSIP-41329-BioExtractor Stage Making Multiple API Calls to IDRepo per Packet Instead of a Single Call
    Signed-off-by: trialblazerseee <84778104+trialblazerseee@users.noreply.github.com>

--- registration-processor/core-processor/registration-processor-biometric-extraction-stage/src/main/java/io/mosip/registration/processor/stages/biometric/extraction/stage/BiometricExtractionStage.java
+++ registration-processor/core-processor/registration-processor-biometric-extraction-stage/src/main/java/io/mosip/registration/processor/stages/biometric/extraction/stage/BiometricExtractionStage.java
@@ -196,11 +196,8 @@ public class BiometricExtractionStage extends MosipVerticleAPIManager{
 			else {
 				ExtractorsDto extractorsDto=getExtractors(registrationStatusDto.getRegistrationId());
 				if(extractorsDto.getExtractors()!=null && !extractorsDto.getExtractors().isEmpty()) {
-				for(ExtractorDto dto:extractorsDto.getExtractors()) {
-					addBiometricExtractiontoIdRepository(dto,registrationStatusDto.getRegistrationId());	
-				}
-				}
-				else {
+					addBiometricExtractiontoIdRepository(extractorsDto.getExtractors(),registrationStatusDto.getRegistrationId());
+				} else {
 					throw new RegistrationProcessorCheckedException(PlatformErrorMessages.RPR_PMS_BIOMETRIC_EXTRACTION_NULL_RESPONSE.getCode(),
 							PlatformErrorMessages.RPR_PMS_BIOMETRIC_EXTRACTION_NULL_RESPONSE.getMessage());
 				}
@@ -348,19 +345,32 @@ public class BiometricExtractionStage extends MosipVerticleAPIManager{
 	 * @throws IdrepoDraftException
 	 *
 	 */
-	private IdResponseDTO addBiometricExtractiontoIdRepository(ExtractorDto dto,
+	private IdResponseDTO addBiometricExtractiontoIdRepository(List<ExtractorDto> dtoList,
 			String registrationId)
 			throws ApisResourceAccessException, IdrepoDraftReprocessableException, IdrepoDraftException {
-		String extractionFormat = "";
-		if(dto.getBiometric().equals("iris")) {
-			extractionFormat="irisExtractionFormat";
-		}if(dto.getBiometric().equals("face")) {
-			extractionFormat="faceExtractionFormat";
-		}if(dto.getBiometric().equals("finger")) {
-			extractionFormat="fingerExtractionFormat";
+		String queryParmeter = new String();
+		String queryValue = new String();
+
+		for(ExtractorDto dto : dtoList) {
+			if(queryParmeter != null) {
+				queryParmeter += ",";
+				queryValue += ",";
+			}
+
+			if(dto.getBiometric().equals("iris")) {
+					queryParmeter += "irisExtractionFormat";
+					queryValue += dto.getAttributeName();
+			} if(dto.getBiometric().equals("face")) {
+					queryParmeter += "faceExtractionFormat";
+					queryValue += dto.getAttributeName();
+			}if (dto.getBiometric().equals("finger")) {
+					queryParmeter += "fingerExtractionFormat";
+					queryValue += dto.getAttributeName();
+			}
 		}
+
 		List<String> segments=List.of(registrationId);
-		IdResponseDTO response= (IdResponseDTO) registrationProcessorRestClientService.putApi(ApiName.IDREPOEXTRACTBIOMETRICS, segments, extractionFormat, dto.getAttributeName(), null, IdResponseDTO.class, null);
+		IdResponseDTO response= (IdResponseDTO) registrationProcessorRestClientService.putApi(ApiName.IDREPOEXTRACTBIOMETRICS, segments, queryParmeter, queryValue, null, IdResponseDTO.class, null);
 
 		if (response.getErrors() != null && !response.getErrors().isEmpty()) {
 			ErrorDTO error = response.getErrors().get(0);
