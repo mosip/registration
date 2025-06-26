3049c8e0958bb991ca0f8bcd68bed41d2ea4833d
kameshsr <47484458+kameshsr@users.noreply.github.com>
2025-06-26 19:38:25 +0530

    [MOSIP-41872] [MOSIP-41321] [MOSIP-41320] [MOSIP-41330] [MOSIP-41329] - Changes related to Performance Tunning (#2102) (#2104)
    * MOSIP-41872 Fixed merge conflict
    
    
    
    * MOSIP-41872 corrected biometric stage
    
    
    
    * MOSIP-41872 Removed unused variable
    
    
    
    * MOSIP-41872 Removed unused variable
    
    
    
    ---------
    
    Signed-off-by: kameshsr <kameshsr1338@gmail.com>
    Co-authored-by: TRIALBLAZERS <84778104+trialblazerseee@users.noreply.github.com>

--- registration-processor/pre-processor/registration-processor-packet-validator-stage/src/main/java/io/mosip/registration/processor/stages/packet/validator/PacketValidateProcessor.java
+++ registration-processor/pre-processor/registration-processor-packet-validator-stage/src/main/java/io/mosip/registration/processor/stages/packet/validator/PacketValidateProcessor.java
@@ -10,8 +10,6 @@ import java.time.format.DateTimeParseException;
 import java.util.Arrays;
 import java.util.Date;
 import java.util.Map;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
 import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang3.exception.ExceptionUtils;
@@ -204,7 +202,7 @@ public class PacketValidateProcessor {
 					// save audit details
 					InternalRegistrationStatusDto finalRegistrationStatusDto = registrationStatusDto;
 					String finalRegistrationId = registrationId;
-					Runnable r = () -> {
+					
 						try {
 							auditUtility.saveAuditDetails(finalRegistrationId,
 									finalRegistrationStatusDto.getRegistrationType());
@@ -214,10 +212,7 @@ public class PacketValidateProcessor {
 									description.getCode() + " Inside Runnable ", "");
 
 						}
-					};
-					ExecutorService es = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
-					es.submit(r);
-					es.close();
+					
 					registrationStatusDto
 							.setLatestTransactionStatusCode(RegistrationTransactionStatusCode.SUCCESS.toString());
 					object.setIsValid(Boolean.TRUE);
