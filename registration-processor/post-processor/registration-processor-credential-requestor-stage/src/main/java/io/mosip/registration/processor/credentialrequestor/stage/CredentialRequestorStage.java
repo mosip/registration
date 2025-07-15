5dd5f1f94a09beabc5f6a725923ab72900696d2d
tarique-azeez <mdtarique2703@gmail.com>
2025-07-15 15:43:48 +0530

    [MOSIP-36457]Reprocessor is unable to process the failed packets in credential-requestor-stage in v1.2.0.1 (#2126)
    * cherry-pick for MOSIP-36457
    
    Signed-off-by: tarique-azeez <mdtarique2703@gmail.com>
    
    * fix conflicts
    
    Signed-off-by: tarique-azeez <mdtarique2703@gmail.com>
    
    ---------
    
    Signed-off-by: tarique-azeez <mdtarique2703@gmail.com>
    Co-authored-by: TRIALBLAZERS <84778104+trialblazerseee@users.noreply.github.com>
    Signed-off-by: Dhanendra Sahu <dhanendra.tech@gmail.com>

--- registration-processor/post-processor/registration-processor-credential-requestor-stage/src/main/java/io/mosip/registration/processor/credentialrequestor/stage/CredentialRequestorStage.java
+++ registration-processor/post-processor/registration-processor-credential-requestor-stage/src/main/java/io/mosip/registration/processor/credentialrequestor/stage/CredentialRequestorStage.java
@@ -784,7 +784,7 @@ public class PrintingStage extends MosipVerticleAPIManager {
 	 */
 	public void deployVerticle() {
 		mosipEventBus = this.getEventBus(this, clusterManagerUrl, workerPoolSize);
-		this.consumeAndSend(mosipEventBus, MessageBusAddress.PRINTING_BUS_IN, MessageBusAddress.PRINTING_BUS_OUT,
+		this.consumeAndSend(mosipEventBus, MessageBusAddress.CREDENTIAL_REQUESTOR_BUS_IN, MessageBusAddress.CREDENTIAL_REQUESTOR_BUS_OUT,
 				messageExpiryTimeLimit);
 	}
 
@@ -798,7 +798,7 @@ public class PrintingStage extends MosipVerticleAPIManager {
 	@Override
 	public MessageDTO process(MessageDTO object) {
 		TrimExceptionMessage trimeExpMessage = new TrimExceptionMessage();
-		object.setMessageBusAddress(MessageBusAddress.PRINTING_BUS_IN);
+		object.setMessageBusAddress(MessageBusAddress.CREDENTIAL_REQUESTOR_BUS_IN);
 		object.setInternalError(Boolean.FALSE);
 		object.setIsValid(Boolean.FALSE);
 		LogDescription description = new LogDescription();
@@ -988,7 +988,7 @@ public class PrintingStage extends MosipVerticleAPIManager {
 	@Override
 	public void start() {
 		router.setRoute(this.postUrl(getVertx(), 
-				MessageBusAddress.PRINTING_BUS_IN, MessageBusAddress.PRINTING_BUS_OUT));
+				MessageBusAddress.CREDENTIAL_REQUESTOR_BUS_IN, MessageBusAddress.CREDENTIAL_REQUESTOR_BUS_OUT));
 		this.createServer(router.getRouter(), getPort());
 	}
 
