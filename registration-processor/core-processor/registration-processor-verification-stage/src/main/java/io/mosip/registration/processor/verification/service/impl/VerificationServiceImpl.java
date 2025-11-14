9f45fda24247a2752bbe3ec1cfbfee8c478e6d98
Chetan Kumar Hirematha <chetankumar.h.239@gmail.com>
2025-11-14 20:41:41 +0530

    Replace DateUtils with DateUtils2
    Signed-off-by: Chetan Kumar Hirematha <chetankumar.h.239@gmail.com>

--- registration-processor/core-processor/registration-processor-verification-stage/src/main/java/io/mosip/registration/processor/verification/service/impl/VerificationServiceImpl.java
+++ registration-processor/core-processor/registration-processor-verification-stage/src/main/java/io/mosip/registration/processor/verification/service/impl/VerificationServiceImpl.java
@@ -12,6 +12,7 @@ import java.util.Map;
 import java.util.UUID;
 import java.util.stream.Collectors;
 
+import io.mosip.kernel.core.util.DateUtils2;
 import io.mosip.registration.processor.core.exception.PacketManagerNonRecoverableException;
 import org.apache.commons.lang.StringUtils;
 import org.apache.commons.lang3.exception.ExceptionUtils;
@@ -657,7 +658,7 @@ public class VerificationServiceImpl implements VerificationService {
 
 		req.setId(VerificationConstants.MANUAL_ADJUDICATION_ID);
 		req.setVersion(VerificationConstants.VERSION);
-		req.setRequesttime(DateUtils.getUTCCurrentDateTimeString(env.getProperty(DATETIME_PATTERN)));
+		req.setRequesttime(DateUtils2.getUTCCurrentDateTimeString(env.getProperty(DATETIME_PATTERN)));
 		req.setReferenceId(messageDTO.getRid());
 		try {
 			req.setReferenceURL(getDataShareUrl(messageDTO.getRid(), registrationStatusDto.getRegistrationType()));
