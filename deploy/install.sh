efdff8b64d745eea330bf65156f068d37f9f10ea
Dhanendra Sahu <dhanendra.tech@gmail.com>
2025-11-16 22:16:46 +0530

    updated tag version
    Signed-off-by: Dhanendra Sahu <dhanendra.tech@gmail.com>

--- deploy/install.sh
+++ deploy/install.sh
@@ -7,8 +7,8 @@ if [ $# -ge 1 ] ; then
 fi
 
 NS=regproc
-CHART_VERSION=1.3.0-beta.1-develop
-SALTGEN_CHART_VERSION=1.3.0-beta.1-develop
+CHART_VERSION=1.3.0-develop
+SALTGEN_CHART_VERSION=1.3.0-develop
 
 echo Create $NS namespace
 kubectl create ns $NS
