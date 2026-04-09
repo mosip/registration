0d5fb2523ce7787521b7e8728b22127c937415f1
kameshsr <kameshsr1338@gmail.com>
2026-04-09 16:34:09 +0530

    MOSIP-44206 Added index
    Signed-off-by: kameshsr <kameshsr1338@gmail.com>

--- db_upgrade_scripts/mosip_regprc/sql/1.3.0_to_1.3.1_rollback.sql
+++ db_upgrade_scripts/mosip_regprc/sql/1.3.0_to_1.3.1_rollback.sql
@@ -1,1 +1,4 @@
-\echo 'Rollback Queries not required for transition from $CURRENT_VERSION to $UPGRADE_VERSION'
+DROP INDEX IF EXISTS regprc.idx_registration_reg_id_is_active;
+DROP INDEX IF EXISTS regprc.idx_registration_reprocessor;
+DROP INDEX IF EXISTS regprc.idx_registration_paused_actionable;
+DROP INDEX IF EXISTS regprc.idx_reg_transaction_reg_id;
