6f37abd3fa653253de06cf77da4117c8d7969b1f
ashok-ksharma <ashok@mosip.io>
2026-05-25 18:24:23 +0530

    Remove unnecessary blank lines in AbisMiddleWareStage
    Signed-off-by: ashok-ksharma <ashok@mosip.io>

--- registration-processor/core-processor/registration-processor-abis-middleware-stage/src/main/java/io/mosip/registartion/processor/abis/middleware/stage/AbisMiddleWareStage.java
+++ registration-processor/core-processor/registration-processor-abis-middleware-stage/src/main/java/io/mosip/registartion/processor/abis/middleware/stage/AbisMiddleWareStage.java
@@ -347,11 +347,9 @@ public class AbisMiddleWareStage extends MosipVerticleAPIManager {
 						.collect(Collectors.toList());
 				validateNullCheck(abisQueue, ABIS_QUEUE_NOT_FOUND);
 				byte[] reqBytearray = abisIdentifyRequest.getReqText();
-
 				boolean isAddedToQueue = sendToQueue(abisQueue.get(0).getMosipQueue(), new String(reqBytearray),
 						abisQueue.get(0).getInboundQueueName(), abisQueue.get(0).getInboundMessageTTL());
 				updateAbisRequest(isAddedToQueue, abisIdentifyRequest, internalRegDto);
-
 			}
 
 		}
@@ -363,11 +361,9 @@ public class AbisMiddleWareStage extends MosipVerticleAPIManager {
 			validateNullCheck(abisQueue, ABIS_QUEUE_NOT_FOUND);
 
 			byte[] reqBytearray = abisInprogressRequest.getReqText();
-
 			boolean isAddedToQueue = sendToQueue(abisQueue.get(0).getMosipQueue(), new String(reqBytearray),
 					abisQueue.get(0).getInboundQueueName(), abisQueue.get(0).getInboundMessageTTL());
 			updateAbisRequest(isAddedToQueue, abisInprogressRequest, internalRegDto);
-
 		}
 		// send all identify requests for already processed insert requests
 		for (AbisRequestDto abisAlreadyProcessedInsertRequest : abisAlreadyprocessedInsertRequestList) {
@@ -382,7 +378,6 @@ public class AbisMiddleWareStage extends MosipVerticleAPIManager {
 			boolean isAddedToQueue = sendToQueue(abisQueue.get(0).getMosipQueue(), new String(reqBytearray),
 					abisQueue.get(0).getInboundQueueName(), abisQueue.get(0).getInboundMessageTTL());
 			updateAbisRequest(isAddedToQueue, identifyRequest.get(0), internalRegDto);
-
 		}
 	}
 
@@ -464,7 +459,6 @@ public class AbisMiddleWareStage extends MosipVerticleAPIManager {
 					boolean isAddedToQueue = sendToQueue(queue, new String(abisIdentifyRequestDto.getReqText()),
 							abisInBoundAddress, inboundMessageTTL);
 					updateAbisRequest(isAddedToQueue, abisIdentifyRequestDto, internalRegStatusDto);
-
 				} else {
 					internalRegStatusDto
 							.setLatestTransactionStatusCode(RegistrationTransactionStatusCode.REPROCESS.toString());
